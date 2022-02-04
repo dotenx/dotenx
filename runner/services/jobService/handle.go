4e5095287157f8db37a1571834756fc980c73588
Armin <armin@Armin.local>
2022-02-04 19:14:14 +0330

    job dto debugged

--- runner/services/jobService/handle.go
+++ runner/services/jobService/handle.go
@@ -1,6 +1,9 @@
 package jobService
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/utopiops/automated-ops/runner/config"
 	"github.com/utopiops/automated-ops/runner/executors"
 	"github.com/utopiops/automated-ops/runner/models"
@@ -10,20 +13,25 @@ import (
 func (manager *JobManager) HandleJob(job models.Job, logHelper shared.LogHelper) {
 	executor := executors.NewExecutor()
 	var name, taskType string
+	var body map[string]interface{}
 	if _, ok := job.Data["name"]; ok {
 		name = job.Data["name"].(string)
 	}
 	if _, ok := job.Data["type"]; ok {
 		taskType = job.Data["type"].(string)
 	}
+	if _, ok := job.Data["body"]; ok {
+		body = job.Data["body"].(models.TaskBody)
+	}
 	taskDetails := models.TaskDetails{
 		Name:           name,
 		Id:             job.Id,
 		Type:           taskType,
-		Body:           job.Data,
+		Body:           body,
 		ServiceAccount: "service?",
 	}
 	result := executor.Execute(&taskDetails)
+	fmt.Println(result)
 	resultDto := models.TaskStatus{
 		ReturnValue: "",
 		Result:      models.Status(result.Status),
@@ -35,5 +43,6 @@ func (manager *JobManager) HandleJob(job models.Job, logHelper shared.LogHelper)
 		resultDto.Result = models.StatusFailed
 	}
 	manager.LogHelper.Log(result.Log, true, result.Id)
+	log.Println(resultDto)
 	manager.SendResult(job.Id, resultDto)
 }
