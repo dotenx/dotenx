aa2ce4d65c21a3a1eb066401396a5940885ccde4
Armin <armin.godarzi@utopiops.com>
2022-02-13 12:17:34 +0330

    endpoint to get task result by task name added

--- ao-api/controllers/execution/task_execution_result.go
+++ ao-api/controllers/execution/task_execution_result.go
@@ -12,6 +12,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (e *ExecutionController) GetTaskExecutionResultByName() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		executionId, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		taskName, err := strconv.Atoi(c.Param("task_name"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		taskId, err := e.Service.GetTaskId(executionId, taskName)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "error in getting taskId, invalid task name"})
+			return
+		}
+
+		res, err := e.Service.GetTaskExecutionResult(executionId, taskId)
+		if err != nil && err.Error() == "Foreign key constraint violence" {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+		c.JSON(http.StatusOK, res)
+	}
+}
 func (e *ExecutionController) GetTaskExecutionResult() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
