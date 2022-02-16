296594235990c33e4f052dd10c4a9808c81698d3
frustak <soulburn007@gmail.com>
2022-02-16 14:19:18 +0330

    Reset task states after run

--- ui/src/containers/action-bar.tsx
+++ ui/src/containers/action-bar.tsx
@@ -1,9 +1,12 @@
 import { css } from '@emotion/react'
 import { useAtom } from 'jotai'
+import { isNode, Node } from 'react-flow-renderer'
 import { useMutation } from 'react-query'
 import { startPipeline } from '../api'
 import { Button } from '../components/button'
 import { Modal } from '../components/modal'
+import { NodeData } from '../components/pipe-node'
+import { flowAtom } from '../hooks/use-flow'
 import { useLayout } from '../hooks/use-layout'
 import { Modals, useModal } from '../hooks/use-modal'
 import { selectedPipelineAtom } from './pipeline-select'
@@ -15,7 +18,22 @@ export function ActionBar() {
 	const { onLayout } = useLayout()
 	const modal = useModal()
 	const [selectedPipeline] = useAtom(selectedPipelineAtom)
-	const mutation = useMutation(startPipeline)
+	const setElements = useAtom(flowAtom)[1]
+
+	const mutation = useMutation(startPipeline, {
+		onSuccess: () => {
+			setElements((elements) =>
+				elements.map((element) => {
+					if (isNode(element)) {
+						const node = element as Node<NodeData>
+						if (!node.data) return node
+						return { ...node, data: { ...node.data, status: undefined } }
+					}
+					return element
+				})
+			)
+		},
+	})
 
 	const onRun = () => {
 		if (selectedPipeline) mutation.mutate({ endpoint: selectedPipeline.endpoint })
