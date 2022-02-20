ce6d5a22bb8c1415979245788fad5589cb711726
Armin <armin.godarzi@utopiops.com>
2022-02-20 23:38:42 +0330

    controller changed

--- ao-api/controllers/integration/controller.go
+++ ao-api/controllers/integration/controller.go
@@ -13,16 +13,20 @@ type IntegrationController struct {
 
 func (controller *IntegrationController) GetIntegrationFields() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		//integrationType := c.Param("type")
+		//accountId := c.MustGet("accountId").(string)
 		c.JSON(http.StatusOK, nil)
 	}
 }
 func (controller *IntegrationController) AddIntegration() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		//accountId := c.MustGet("accountId").(string)
 		c.JSON(http.StatusOK, nil)
 	}
 }
 func (controller *IntegrationController) GetIntegrations() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		//accountId := c.MustGet("accountId").(string)
 		c.JSON(http.StatusOK, nil)
 	}
 }
