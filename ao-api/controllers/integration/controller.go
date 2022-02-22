92719f56c1e3e21be71eed9b559feed906bcb844
Armin <armin.godarzi@utopiops.com>
2022-02-22 13:53:15 +0330

    sendGrid example added

--- ao-api/controllers/integration/controller.go
+++ ao-api/controllers/integration/controller.go
@@ -71,6 +71,7 @@ func (controller *IntegrationController) AddIntegration() gin.HandlerFunc {
 func (controller *IntegrationController) GetIntegrationTypes() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		integrations, _ := controller.Service.GetIntegrationTypes()
+		//fmt.Println(integrations)
 		c.JSON(http.StatusOK, integrations)
 	}
 }
