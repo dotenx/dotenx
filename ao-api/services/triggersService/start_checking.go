17fc30c491a1eb2ee9b01db87673d56637d5dd5a
Armin <armin.godarzi@utopiops.com>
2022-02-27 13:58:25 +0330

    docker client added

--- ao-api/services/triggersService/start_checking.go
+++ ao-api/services/triggersService/start_checking.go
@@ -2,8 +2,11 @@ package triggerService
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"time"
+
+	"github.com/docker/docker/client"
 
 	"github.com/utopiops/automated-ops/ao-api/config"
 	"github.com/utopiops/automated-ops/ao-api/models"
@@ -19,22 +22,27 @@ func (manager *TriggerManager) StartChecking(accId string, store integrationStor
 	if err != nil {
 		return err
 	}
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		fmt.Println("Unable to create docker client")
+		return err
+	}
 	for _, trigger := range triggers {
-		go handleTrigger(accId, trigger, store, freq)
+		go cli.handleTrigger(accId, trigger, store, freq)
 	}
 	return nil
 }
 
-func handleTrigger(accountId string, trigger models.EventTrigger, store integrationStore.IntegrationStore, freq int) {
+func (dockerClient *client.Client) handleTrigger(accountId string, trigger models.EventTrigger, store integrationStore.IntegrationStore, freq int) {
 	integration, err := store.GetIntegrationsByName(context.Background(), accountId, trigger.Integration)
 	if err != nil {
 		return
 	}
 	for start := time.Now(); time.Since(start) < time.Duration(freq)*time.Second; {
-		checkTrigger(trigger, integration)
+		dockerClient.checkTrigger(trigger, integration)
 	}
 }
 
-func checkTrigger(trigger models.EventTrigger, integration models.Integration) {
+func (dockerClient *client.Client) checkTrigger(trigger models.EventTrigger, integration models.Integration) {
 
 }
