22e62a6a69c507621f3cad9b9986895cc9c0ae1f
Armin <armin.godarzi@utopiops.com>
2022-02-27 13:49:02 +0330

    freq to handle trigger added

--- ao-api/services/triggersService/start_checking.go
+++ ao-api/services/triggersService/start_checking.go
@@ -2,8 +2,10 @@ package triggerService
 
 import (
 	"context"
+	"strconv"
 	"time"
 
+	"github.com/utopiops/automated-ops/ao-api/config"
 	"github.com/utopiops/automated-ops/ao-api/models"
 	"github.com/utopiops/automated-ops/ao-api/stores/integrationStore"
 )
@@ -13,18 +15,23 @@ func (manager *TriggerManager) StartChecking(accId string, store integrationStor
 	if err != nil {
 		return err
 	}
+	freq, err := strconv.Atoi(config.Configs.App.CheckTrigger)
+	if err != nil {
+		return err
+	}
 	for _, trigger := range triggers {
-		go handleTrigger(accId, trigger, store)
+		go handleTrigger(accId, trigger, store, freq)
 	}
 	return nil
 }
 
-func handleTrigger(accountId string, trigger models.EventTrigger, store integrationStore.IntegrationStore) {
+func handleTrigger(accountId string, trigger models.EventTrigger, store integrationStore.IntegrationStore, freq int) {
 	integration, err := store.GetIntegrationsByName(context.Background(), accountId, trigger.Integration)
 	if err != nil {
 		return
 	}
-	for start := time.Now(); time.Since(start) < time.Duration()*time.Second; {
+
+	for start := time.Now(); time.Since(start) < time.Duration(freq)*time.Second; {
 		checkTrigger()
 	}
 }
