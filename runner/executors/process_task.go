f5641913894f23a444b362c9e77308dee4707f64
Armin <armin.godarzi@utopiops.com>
2022-03-05 19:18:15 +0330

    process task refactored

--- runner/executors/process_task.go
+++ runner/executors/process_task.go
@@ -12,45 +12,24 @@ func ProcessTask(task *models.TaskDetails) (processedTask *models.Task) {
 	processedTask = &models.Task{}
 	processedTask.Detailes = *task
 	processedTask.IsPredifined = true
-	switch task.Type {
-	case "HttpCall":
-		var body string
-		if b, ok := task.Body["body"]; ok {
-			body = fmt.Sprintf("%v", b)
+	if task.Type == "runImage" {
+		processedTask.IsPredifined = false
+		processedTask.Detailes.Image = task.Body["image"].(string)
+		processedTask.Script = strings.Split(task.Body["script"].(string), " ")
+	} else {
+		envs := make([]string, 0)
+		for _, field := range models.AvaliableTasks[task.Type].Fields {
+			if value, ok := task.Body[field.Key]; ok {
+				var envVar string
+				if field.Type == "text" {
+					envVar = field.Key + "=" + value.(string)
+				} else {
+					envVar = field.Key + "=" + fmt.Sprintf("%v", value)
+				}
+				envs = append(envs, envVar)
+			}
 		}
-		processedTask.EnvironmentVariables = []string{"method=" + task.Body["method"].(string),
-			"url=" + task.Body["url"].(string),
-			"body=" + body}
-	/*case "CreateAccount":*/
-	case "GitlabAddMember":
-		processedTask.EnvironmentVariables = []string{"privateToken=" + task.Body["privateToken"].(string),
-			"id=" + task.Body["id"].(string),
-			"userId=" + task.Body["userId"].(string),
-			"accessLevel=" + task.Body["accessLevel"].(string),
-			"expiresAt=" + task.Body["expiresAt"].(string),
-			"type=" + task.Body["type"].(string),
-			"action=" + "add"}
-	case "GitlabRemoveMember":
-		processedTask.EnvironmentVariables = []string{"privateToken=" + task.Body["privateToken"].(string),
-			"id=" + task.Body["id"].(string),
-			"userId=" + task.Body["userId"].(string),
-			"type=" + task.Body["type"].(string),
-			"action=" + "remove"}
-	case "default":
-		{
-			processedTask.IsPredifined = false
-			processedTask.Detailes.Image = task.Body["image"].(string)
-			processedTask.Script = strings.Split(task.Body["script"].(string), " ")
-		}
+		processedTask.EnvironmentVariables = envs
 	}
 	return
-}
-
-func IsTaskTypeValid(taskType string) bool {
-	for t := range models.AvaliableTasks {
-		if t == taskType {
-			return true
-		}
-	}
-	return false
 }
