febd231cf35d09cd4a60e166e5110e75baa37a23
Armin <armin.godarzi@utopiops.com>
2022-03-06 21:40:12 +0330

    meta data added to task detailes

--- runner/models/task.go
+++ runner/models/task.go
@@ -1,19 +1,14 @@
 package models
 
-import (
-	"io/ioutil"
-
-	"gopkg.in/yaml.v2"
-)
-
 type TaskDetails struct {
-	Name           string   `json:"name"`
-	Id             int      `json:"id"`
-	Type           string   `json:"type"`
-	Image          string   `json:"image"`
-	Timeout        int      `json:"timeout"`
-	Body           TaskBody `json:"body"`
-	ServiceAccount string   `json:"serviceAccount"`
+	Name           string       `json:"name"`
+	Id             int          `json:"id"`
+	Type           string       `json:"type"`
+	Image          string       `json:"image"`
+	Timeout        int          `json:"timeout"`
+	Body           TaskBody     `json:"body"`
+	ServiceAccount string       `json:"serviceAccount"`
+	MetaData       TaskMetaData `json:"task_meta_data"`
 }
 
 type TaskBody map[string]interface{}
@@ -41,37 +36,13 @@ type Task struct {
 	IsPredifined         bool
 }
 
-type TaskDefinition struct {
+type TaskMetaData struct {
 	Type   string
 	Fields []TaskField
 	Image  string
 }
 
-var AvaliableTasks map[string]TaskDefinition
-
 type TaskField struct {
 	Key  string `json:"key"`
 	Type string `json:"type"`
-}
-
-func init() {
-	AvaliableTasks = make(map[string]TaskDefinition)
-	address := "tasks"
-	files, err := ioutil.ReadDir(address)
-	if err != nil {
-		panic(err)
-	}
-	for _, file := range files {
-		var yamlFile TaskDefinition
-		yamlData, err := ioutil.ReadFile(address + "/" + file.Name())
-		if err != nil {
-			panic(err)
-		}
-		err = yaml.Unmarshal(yamlData, &yamlFile)
-		if err != nil {
-			panic(err)
-		}
-
-		AvaliableTasks[yamlFile.Type] = yamlFile
-	}
 }
