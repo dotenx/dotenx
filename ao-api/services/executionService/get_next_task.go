38987ddf838d498e951f7fb17ec404d976ff3ba0
Armin <armin.godarzi@utopiops.com>
2022-03-13 16:30:24 +0330

    cleaning up get next task method

--- ao-api/services/executionService/get_next_task.go
+++ ao-api/services/executionService/get_next_task.go
@@ -9,14 +9,24 @@ import (
 	"github.com/utopiops/automated-ops/ao-api/models"
 )
 
-func (manager *executionManager) GetNextTask(taskId, executionId int, status, accountId string) error {
+func (manager *executionManager) GetNextTask(taskId, executionId int, status, accountId string) (err error) {
+	taskIds := make([]int, 0)
 	if taskId <= 0 {
-		fmt.Println("taskId <= 0", executionId)
-		taskId, err := manager.Store.GetInitialTask(noContext, executionId)
+		taskId, err = manager.Store.GetInitialTask(noContext, executionId)
 		if err != nil {
 			log.Println(err.Error())
-			return err
+			return
 		}
+		taskIds = append(taskIds, taskId)
+	} else {
+		taskIds, err = manager.Store.GetNextTasks(noContext, executionId, taskId, status)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+	}
+
+	for _, taskId := range taskIds {
 		task, err := manager.Store.GetTaskByExecution(noContext, executionId, taskId)
 		if err != nil {
 			return err
@@ -57,57 +67,8 @@ func (manager *executionManager) GetNextTask(taskId, executionId int, status, ac
 			return err
 		}
 		manager.UtopiopsService.IncrementUsedTimes(models.AvaliableTasks[task.Type].Author, "task", task.Type)
-		return nil
-	} else {
-		taskIds, err := manager.Store.GetNextTasks(noContext, executionId, taskId, status)
-		if err != nil {
-			log.Println(err.Error())
-			return err
-		}
-		for _, taskId := range taskIds {
-			task, err := manager.Store.GetTaskByExecution(noContext, executionId, taskId)
-			if err != nil {
-				return err
-			}
-			image := models.AvaliableTasks[task.Type].Image
-			jobDTO := job{
-				ExecutionId: executionId,
-				TaskId:      task.Id,
-				Type:        task.Type,
-				Timeout:     task.Timeout,
-				Image:       image,
-				Body:        task.Body,
-				Name:        task.Name,
-				AccountId:   accountId,
-				MetaData:    models.AvaliableTasks[task.Type],
-			}
-			if task.Integration != "" {
-				integration, err := manager.IntegrationService.GetIntegrationByName(accountId, task.Integration)
-				if err != nil {
-					return err
-				}
-				jobDTO.Body["INTEGRATION_ACCESS_TOKEN"] = integration.AccessToken
-				jobDTO.Body["INTEGRATION_URL"] = integration.Url
-				jobDTO.Body["INTEGRATION_KEY"] = integration.Key
-				jobDTO.Body["INTEGRATION_SECRET"] = integration.Secret
-				jobDTO.MetaData.Fields = append(jobDTO.MetaData.Fields, models.TaskField{Key: "INTEGRATION_URL", Type: "text"})
-				jobDTO.MetaData.Fields = append(jobDTO.MetaData.Fields, models.TaskField{Key: "INTEGRATION_SECRET", Type: "text"})
-				jobDTO.MetaData.Fields = append(jobDTO.MetaData.Fields, models.TaskField{Key: "INTEGRATION_KEY", Type: "text"})
-				jobDTO.MetaData.Fields = append(jobDTO.MetaData.Fields, models.TaskField{Key: "INTEGRATION_ACCESS_TOKEN", Type: "text"})
-			}
-			body, err := manager.mapFields(executionId, accountId, jobDTO.Body)
-			if err == nil {
-				jobDTO.Body = body
-			}
-			err = manager.QueueService.QueueTasks(accountId, "default", jobDTO)
-			if err != nil {
-				log.Println(err.Error())
-				return err
-			}
-			manager.UtopiopsService.IncrementUsedTimes(models.AvaliableTasks[task.Type].Author, "task", task.Type)
-		}
-		return nil
 	}
+	return nil
 }
 
 type job struct {
