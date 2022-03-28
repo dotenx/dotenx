7ab04d7ba88cbd5847769718b590324d5c888732
Armin <armin.godarzi@utopiops.com>
2022-03-29 01:10:50 +0430

    get account id from session added

--- ao-api/pkg/utils/utils.go
+++ ao-api/pkg/utils/utils.go
@@ -14,5 +14,11 @@ func FailOnError(err error, msg string) {
 }
 
 func GetAccountId(c *gin.Context) (string, error) {
+	/*session := sessions.Default(c)
+	accountId, ok := session.Get("accountId").(bool)
+	if !ok {
+		return "", errors.New("you need to login first")
+	}
+	return accountId, nil*/
 	return config.Configs.App.AccountId, nil
 }
