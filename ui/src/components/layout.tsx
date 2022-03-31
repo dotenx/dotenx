7cc4fd7ed6895610e0c3939c41ad69e0a1079072
Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>
2022-03-31 20:51:59 +1100

    Rename the name in the license, add the logo

--- ui/src/components/layout.tsx
+++ ui/src/components/layout.tsx
@@ -7,6 +7,11 @@ import ReactModal from 'react-modal'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Link } from 'react-router-dom'
 
+// import logo '../assets/logo.jpg'
+
+import Logo from '../assets/images/logo.png'
+
+
 const theme: Theme = {
 	color: {
 		primary: '#e85d04',
@@ -45,7 +50,7 @@ export function Layout({ children, pathname, header = null }: LayoutProps) {
 						display: 'flex',
 					})}
 				>
-					<h1
+					<div
 						css={{
 							borderRight: '1px solid',
 							borderColor: theme.color.text,
@@ -55,8 +60,11 @@ export function Layout({ children, pathname, header = null }: LayoutProps) {
 							fontWeight: 100,
 						}}
 					>
-						Automated Ops
-					</h1>
+						<img src={Logo} alt="logo" css={{
+							height: '5vh',
+							width: 'auto',
+						}} ></img>
+					</div>
 					<div css={{ flexGrow: '1' }}>{header}</div>
 				</div>
 				<div css={{ flexGrow: '1', display: 'flex' }}>
