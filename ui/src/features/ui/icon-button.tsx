1e724dd0c805cbadbdac9f75afc066604770c22b
frustak <soulburn007@gmail.com>
2022-04-17 14:10:31 +0430

    Improve icon button

--- ui/src/features/ui/icon-button.tsx
+++ ui/src/features/ui/icon-button.tsx
@@ -8,14 +8,15 @@ export function IconButton(props: IconButtonProps) {
 	return (
 		<div className="relative group">
 			<button
-				className="flex items-center justify-center p-1 text-xl rounded hover:bg-gray-100 disabled:hover:bg-white disabled:text-gray-400 disabled:cursor-not-allowed"
+				className="flex items-center justify-center p-1 text-2xl transition rounded hover:bg-gray-100 disabled:hover:bg-transparent disabled:text-gray-400 disabled:cursor-not-allowed"
 				{...props}
 			>
 				{props.children}
 			</button>
 			{props.tooltip && !props.disabled && (
-				<div className="absolute hidden px-2 py-1 mt-2 text-xs text-white bg-gray-900 rounded group-hover:block left-[50%] -translate-x-[50%]">
+				<div className="absolute -left-14 z-10 hidden px-2 py-1 font-semibold text-xs text-white bg-rose-600 rounded group-hover:flex items-center top-[50%] -translate-y-[50%]">
 					{props.tooltip}
+					<div className="absolute w-2 h-2 rotate-45 bg-rose-600 rounded-sm -right-0.5 -z-10" />
 				</div>
 			)}
 		</div>
