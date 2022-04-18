4fad4fe5b68a377035e5b39c8f12f4ececc9a13c
frustak <soulburn007@gmail.com>
2022-04-18 15:40:45 +0430

    Redesgin field

--- ui/src/features/ui/field.tsx
+++ ui/src/features/ui/field.tsx
@@ -12,8 +12,8 @@ interface FieldProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export function Field({ label, errors, control, ...rest }: FieldProps) {
 	return (
-		<div className="flex flex-col gap-0.5">
-			<label htmlFor={rest.name} className="text-sm">
+		<div className="flex flex-col gap-1">
+			<label htmlFor={rest.name} className="text-sm font-bold">
 				{label}
 			</label>
 			<Controller
@@ -22,7 +22,7 @@ export function Field({ label, errors, control, ...rest }: FieldProps) {
 				defaultValue="" // This is a bit tricky, maybe needs to change for different value types in future
 				render={({ field: { onChange, value, name, ref } }) => (
 					<input
-						className="px-2 py-1 border border-black rounded"
+						className="px-2 py-1 border rounded-lg border-slate-400 placeholder:text-slate-500 outline-rose-500 focus:ring-0 focus:border-slate-400 form-input focus:outline outline-2 outline-offset-[-1px]"
 						id={rest.name}
 						onChange={onChange}
 						value={value}
