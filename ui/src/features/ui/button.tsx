91866319c8644e1dec5c04cac964034715861d0d
frustak <soulburn007@gmail.com>
2022-04-19 15:53:29 +0430

    refactor automations, integrations and triggers pages to use general button component

--- ui/src/features/ui/button.tsx
+++ ui/src/features/ui/button.tsx
@@ -7,7 +7,7 @@ export function Button({ className, ...rest }: ButtonProps) {
 	return (
 		<button
 			className={clsx(
-				'flex items-center justify-center w-full py-2 font-medium text-white bg-rose-600 transition  rounded-lg cursor-pointer disabled:cursor-not-allowed hover:bg-rose-700 focus:bg-rose-700 outline-rose-500 outline-offset-4 whitespace-nowrap',
+				'flex items-center justify-center gap-1 w-full py-2 px-3 font-medium text-white bg-rose-600 transition  rounded-lg cursor-pointer disabled:cursor-not-allowed hover:bg-rose-700 focus:bg-rose-700 outline-rose-500 outline-offset-4 whitespace-nowrap',
 				className
 			)}
 			{...rest}
