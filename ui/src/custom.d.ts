fdf7c0f888f3156f94c075b35f2cd986625f142d
frustak <soulburn007@gmail.com>
2022-04-19 12:09:35 +0430

    Add svg type

---
+++ ui/src/custom.d.ts
@@ -0,0 +1,15 @@
+declare module '*.png' {
+	const value: any
+	export = value
+}
+
+declare module '*.svg' {
+	import * as React from 'react'
+
+	export const ReactComponent: React.FunctionComponent<
+		React.SVGProps<SVGSVGElement> & { title?: string }
+	>
+
+	const src: string
+	export default src
+}
