cb92aed778eecbac5e9983e7b20700ccc4f55ffe
frustak <soulburn007@gmail.com>
2022-05-23 15:12:03 +0430

    Implement logout

--- ui/src/features/ui/sidebar.tsx
+++ ui/src/features/ui/sidebar.tsx
@@ -1,6 +1,10 @@
 import { memo } from 'react'
 import { BsFillCalendar3WeekFill, BsFillDiagram3Fill, BsHddNetworkFill } from 'react-icons/bs'
+import { IoExit } from 'react-icons/io5'
+import { useMutation } from 'react-query'
+import { logout } from '../../api/admin'
 import logo from '../../assets/images/logo.png'
+import { IS_LOCAL } from '../../constants'
 import { NavItem } from './nav-item'
 
 const items = [
@@ -16,18 +20,43 @@ export const Sidebar = memo(() => {
 				<img className="w-10 rounded" src={logo} alt="logo" />
 				<span className="transition opacity-0 group-hover:opacity-100">DoTenX</span>
 			</div>
-			<div className="flex flex-col gap-6 mt-20">
-				{items.map((item) => (
-					<NavItem key={item.label} to={item.to}>
-						<span className="text-xl">{item.icon}</span>
-						<span className="transition opacity-0 group-hover:opacity-100">
-							{item.label}
-						</span>
-					</NavItem>
-				))}
+			<div className="flex flex-col justify-between grow">
+				<div className="flex flex-col gap-6 mt-20">
+					{items.map((item) => (
+						<NavItem key={item.label} to={item.to}>
+							<span className="text-xl">{item.icon}</span>
+							<span className="transition opacity-0 group-hover:opacity-100">
+								{item.label}
+							</span>
+						</NavItem>
+					))}
+				</div>
+
+				{!IS_LOCAL && <Logout />}
 			</div>
 		</div>
 	)
 })
+
+function Logout() {
+	const logoutMutation = useMutation(logout, {
+		onSuccess: () => window.location.replace('https://admin.dotenx.com'),
+	})
+
+	return (
+		<button
+			className="flex items-center justify-between h-8 gap-6 py-6 pl-1 pr-8 transition outline-rose-500 hover:bg-rose-500 focus:bg-rose-500"
+			onClick={() => logoutMutation.mutate()}
+		>
+			<div className="w-1 h-8 transition rounded-sm shrink-0" />
+			<div className="flex items-center gap-4 grow">
+				<span className="text-xl">
+					<IoExit />
+				</span>
+				<span className="transition opacity-0 group-hover:opacity-100">Logout</span>
+			</div>
+		</button>
+	)
+}
 
 Sidebar.displayName = 'Sidebar'
