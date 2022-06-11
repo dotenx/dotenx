68a00e3cc2faf0ee04980a5da6d72b2991a2c9e3
frustak <soulburn007@gmail.com>
2022-06-11 20:18:39 +0430

    Fix disabled button border color

--- ui/src/features/ui/button.tsx
+++ ui/src/features/ui/button.tsx
@@ -9,7 +9,7 @@ export function Button({ variant = 'filled', className, ...rest }: ButtonProps) 
 	return (
 		<button
 			className={clsx(
-				'flex items-center justify-center gap-1 w-full py-1.5 px-3 font-medium text-white transition rounded-lg cursor-pointer disabled:cursor-not-allowed outline-rose-500 outline-offset-4 whitespace-nowrap disabled:bg-gray-400 border border-rose-600',
+				'flex items-center justify-center gap-1 w-full py-1.5 px-3 font-medium text-white transition rounded-lg cursor-pointer disabled:cursor-not-allowed outline-rose-500 outline-offset-4 whitespace-nowrap disabled:bg-gray-400 border border-rose-600 disabled:border-gray-400',
 				variant === 'filled' && 'bg-rose-600 hover:bg-rose-700 focus:bg-rose-700',
 				variant === 'outlined' &&
 					'bg-white hover:bg-rose-50 focus:bg-rose-50 text-rose-600',
