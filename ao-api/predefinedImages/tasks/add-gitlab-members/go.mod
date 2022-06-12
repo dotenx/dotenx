0cd26cf9b8f72a50c803a95cd4815d592949df36
Hojjat Hajizadeh <hojjat.hajizadeh@utopiops.com>
2022-06-12 16:52:38 +0000

    Delete all files and folder to preparing for merge new MR

--- ao-api/predefinedImages/tasks/add-gitlab-members/go.mod
+++
@@ -1,3 +0,0 @@
-module UTOPIOPS-TEST
-
-go 1.13
--- ao-api/services/executionService/getExecution.go
+++
@@ -1,6 +0,0 @@
-package executionService
-
-func (manager *executionManager) GetExecution(accountId string) (interface{}, error) {
-	//return manager.redisQueue.GetExecution(accountId)
-	return nil, nil
-}
