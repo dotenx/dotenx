0efbd4506718279eab261c682e468788d765a8b3
mehdi65 <mehdi@utopiops.com>
2022-06-19 09:57:49 +0430

    merge master

---
+++ node_modules/googleapis/build/src/apis/cloudsupport/index.d.ts
@@ -0,0 +1,12 @@
+/*! THIS FILE IS AUTO-GENERATED */
+import { AuthPlus } from 'googleapis-common';
+import { cloudsupport_v2beta } from './v2beta';
+export declare const VERSIONS: {
+    v2beta: typeof cloudsupport_v2beta.Cloudsupport;
+};
+export declare function cloudsupport(version: 'v2beta'): cloudsupport_v2beta.Cloudsupport;
+export declare function cloudsupport(options: cloudsupport_v2beta.Options): cloudsupport_v2beta.Cloudsupport;
+declare const auth: AuthPlus;
+export { auth };
+export { cloudsupport_v2beta };
+export { AuthPlus, GlobalOptions, APIRequestContext, GoogleConfigurable, StreamMethodOptions, GaxiosPromise, MethodOptions, BodyResponseCallback, } from 'googleapis-common';
