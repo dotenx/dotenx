94bb65ba317942755a45c3638a63ba5b319d256a
Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>
2022-06-22 20:19:06 +1000

    Fix the comment

--- ao-api/services/databaseService/insert_row.go
+++ ao-api/services/databaseService/insert_row.go
@@ -5,6 +5,6 @@ import "context"
 func (ds *databaseService) InsertRow(projectTag string, tableName string, row map[string]string) error {
 	noContext := context.Background()
 
-	// Add table column to database
+	// Insert a row to table
 	return ds.Store.InsertRow(noContext, projectTag, tableName, row)
 }
