f4f1f7abb03f0ad9289d5f0b62c006da4ccd2333
frustak <soulburn007@gmail.com>
2022-06-23 02:50:52 +0430

    Add project deletion button

--- ui/src/pages/project.tsx
+++ ui/src/pages/project.tsx
@@ -1,20 +1,23 @@
-import { useQuery } from 'react-query'
 import { Navigate, useParams } from 'react-router-dom'
-import { getProject, QueryKey } from '../api'
 import { TableForm, TableList } from '../features/database'
 import { Modals } from '../features/hooks'
+import { ProjectDeletion } from '../features/project'
 import { ContentWrapper, Heading, Modal } from '../features/ui'
 
 export default function ProjectPage() {
 	const { name } = useParams()
-	useQuery(QueryKey.GetProject, () => getProject(name ?? ''), { enabled: !!name })
-
 	if (!name) return <Navigate to="/builder/projects" replace />
+	return <Project name={name} />
+}
 
+function Project({ name }: { name: string }) {
 	return (
 		<>
 			<ContentWrapper>
-				<Heading>Project {name}</Heading>
+				<div className="flex items-center justify-between">
+					<Heading>Project {name}</Heading>
+					<ProjectDeletion name={name} />
+				</div>
 				<TableList projectName={name} />
 			</ContentWrapper>
 			<Modal kind={Modals.NewTable} fluid title="New Table">
