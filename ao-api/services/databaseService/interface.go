a2972cd8c3f8d618a2d3211d12500739b7c0de6c
Mohsen Kamrani <m.kamrani@hotmail.com>
2022-06-24 15:23:21 +1000

    Implement the endpoint to return the list of columns of  a table (#124)
    Co-authored-by: Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>

--- ao-api/services/databaseService/interface.go
+++ ao-api/services/databaseService/interface.go
@@ -11,11 +11,12 @@ type DatabaseService interface {
 	DeleteTable(accountId string, projectName string, tableName string) error
 	AddTableColumn(accountId string, projectName string, tableName string, columnName string, columnType string) error
 	DeleteTableColumn(accountId string, projectName string, tableName string, columnName string) error
+	GetTablesList(accountId string, projectName string) ([]string, error)
+	ListTableColumns(accountId string, projectName string, tableName string) ([]string, error)
 
 	InsertRow(projectTag string, tableName string, row map[string]string) error
 	DeleteRow(projectTag string, tableName string, id int) error
 	SelectRows(projectTag string, tableName string, columns []string, page int, size int) ([]map[string]interface{}, error)
-	GetTablesList(accountId string, projectName string) ([]string, error)
 }
 
 func NewDatabaseService(store databaseStore.DatabaseStore) DatabaseService {
