cf451c78ae8ac6208a8e092612014e96bf9dffd0
erfanqasemizade <erfan.qasemizade@utopiops.com>
2022-06-26 11:24:52 +0430

    Refactor automations page

--- ui/src/pages/automations.tsx
+++ ui/src/pages/automations.tsx
@@ -1,90 +1,20 @@
-import { IoAdd, IoCodeDownload } from 'react-icons/io5'
 import { useQuery } from 'react-query'
-import { Link } from 'react-router-dom'
-import { getAutomations, QueryKey } from '../api'
-import { useDeleteAutomation } from '../features/automation/use-delete'
-import { useNewAutomation } from '../features/automation/use-new'
-import { Button, DeleteButton, Table } from '../features/ui'
+import { Automation, getAutomations, QueryKey } from '../api'
+import { AutomationList } from '../features/automation'
 
 export default function AutomationsPage() {
 	const automationsQuery = useQuery(QueryKey.GetAutomations, getAutomations)
-	const automations = automationsQuery.data?.data
+	const automations = automationsQuery.data?.data.filter(isAutomation)
 
 	return (
-		<div className="grow">
-			<div className="px-32 py-16">
-				<Table
-					title="Automations"
-					emptyText="You have no automation yet, try adding one."
-					loading={automationsQuery.isLoading}
-					actionBar={<NewAutomation />}
-					columns={[
-						{
-							Header: 'Name',
-							accessor: 'name',
-							Cell: ({ value }: { value: string }) => (
-								<Link
-									className="hover:underline underline-offset-2"
-									to={`/automations/${value}`}
-								>
-									{value}
-								</Link>
-							),
-						},
-						{
-							Header: 'Status',
-							accessor: 'is_active',
-							Cell: ({ value }: { value: boolean }) =>
-								value ? (
-									<span className="px-2 py-1 text-xs font-extrabold text-green-600 rounded-md bg-green-50">
-										Active
-									</span>
-								) : (
-									<span className="px-2 py-1 text-xs font-extrabold text-gray-600 rounded-md bg-gray-50">
-										Inactive
-									</span>
-								),
-						},
-						{
-							Header: 'Action',
-							id: 'action',
-							accessor: 'name',
-							Cell: DeletionCell,
-						},
-					]}
-					data={automations}
-				/>
-			</div>
-		</div>
-	)
-}
-
-function NewAutomation() {
-	const newAutomation = useNewAutomation()
-
-	return (
-		<div className="flex gap-4">
-			<Link to="/automations/yaml/import">
-				<Button className="max-w-min">
-					<IoCodeDownload className="text-2xl" />
-					Import YAML
-				</Button>
-			</Link>
-			<Button className="max-w-min" onClick={newAutomation}>
-				<IoAdd className="text-2xl" />
-				New Automation
-			</Button>
-		</div>
-	)
-}
-
-function DeletionCell({ value }: { value: string }) {
-	const deleteMutation = useDeleteAutomation()
-
-	return (
-		<DeleteButton
-			loading={deleteMutation.isLoading}
-			onClick={() => deleteMutation.mutate(value)}
+		<AutomationList
+			automations={automations}
+			loading={automationsQuery.isLoading}
+			title="Automations"
+			kind="automation"
 		/>
 	)
 }
+
+const isAutomation = (automation: Automation) =>
+	!automation.is_template && !automation.is_interaction
