0171106445e063debb91fa129cc4baf7ef5796a3
erfanqasemizade <erfan.qasemizade@utopiops.com>
2022-06-26 11:42:54 +0430

    Refactor automation page

--- ui/src/pages/automation.tsx
+++ ui/src/pages/automation.tsx
@@ -1,54 +1,7 @@
-import { useSetAtom } from 'jotai'
-import { ReactFlowProvider } from 'react-flow-renderer'
-import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
-import { getAutomation, QueryKey } from '../api'
-import { selectedAutomationAtom, selectedAutomationDataAtom } from '../features/atoms'
-import { ActionBar } from '../features/automation'
-import { Flow } from '../features/flow'
-import { Loader } from '../features/ui'
+import { Automation } from '../features/automation'
 
 export default function AutomationPage() {
-	return (
-		<ReactFlowProvider>
-			<Content />
-		</ReactFlowProvider>
-	)
-}
-
-function Content() {
 	const { name } = useParams()
-	const setSelectedAutomation = useSetAtom(selectedAutomationAtom)
-	const setSelected = useSetAtom(selectedAutomationDataAtom)
-	const { isLoading } = useQuery(
-		[QueryKey.GetAutomation, name],
-		() => {
-			if (!name) return
-			return getAutomation(name)
-		},
-		{
-			enabled: !!name,
-			onSuccess: (data) => {
-				if (!name || !data) return
-				setSelectedAutomation(data.data)
-				setSelected({ name, endpoint: data?.data.endpoint, is_active: data.data.is_active })
-			},
-		}
-	)
-
-	if (isLoading)
-		return (
-			<div className="flex justify-center grow">
-				<Loader />
-			</div>
-		)
-
-	return (
-		<>
-			<ActionBar automationName={name} />
-			<div className="flex gap-2 grow">
-				<Flow />
-			</div>
-		</>
-	)
+	return <Automation name={name} kind="automation" />
 }
