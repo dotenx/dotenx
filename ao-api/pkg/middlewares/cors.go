32075e723e8bf2bf8faecdfedacd2ee85bf66628
Hojjat <hojjat.hajizadeh@utopiops.com>
2022-06-28 02:38:30 +0430

    Fix some bugs

--- ao-api/pkg/middlewares/cors.go
+++ ao-api/pkg/middlewares/cors.go
@@ -7,7 +7,15 @@ import (
 // allowedOrigins is config.Configs.App.AllowedOrigins in each service
 func CORSMiddleware(allowedOrigins string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
+		origin := c.Request.Header.Get("Origin")
+		// if utils.ContainsString(strings.Split(allowedOrigins, ","), origin) {
+		// c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		// }
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
+		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "sentry-trace, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Set-Cookie, Cookie")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
