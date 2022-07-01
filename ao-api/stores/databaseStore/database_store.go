d1f73c7bc8a552a92c3df038196c92bd35212aa8
Hojjat <hojjat.hajizadeh@utopiops.com>
2022-07-02 01:23:22 +0430

    Add an endpoint to get user profile and fix bug of insert row endpoint

--- ao-api/stores/databaseStore/database_store.go
+++ ao-api/stores/databaseStore/database_store.go
@@ -18,7 +18,7 @@ type DatabaseStore interface {
 	GetTablesList(ctx context.Context, accountId string, projectName string) ([]string, error)
 	ListTableColumns(ctx context.Context, accountId string, projectName string, tableName string) ([]string, error)
 
-	InsertRow(ctx context.Context, projectTag string, tableName string, row map[string]string) error
+	InsertRow(ctx context.Context, projectTag string, tableName string, row map[string]interface{}) error
 	UpdateRow(ctx context.Context, projectTag string, tableName string, id int, row map[string]string) error
 	DeleteRow(ctx context.Context, projectTag string, tableName string, id int) error
 	SelectRows(ctx context.Context, projectTag string, tableName string, columns []string, offset int, size int) ([]map[string]interface{}, error)
