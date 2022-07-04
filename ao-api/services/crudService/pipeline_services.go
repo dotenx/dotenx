8f855948d0590d4197d1f70321cc490d678bba7d
Armin <armin.godarzi@utopiops.com>
2022-07-05 01:14:49 +0430

    done with refactoring crud services

--- ao-api/services/crudService/pipeline_services.go
+++ ao-api/services/crudService/pipeline_services.go
@@ -9,45 +9,10 @@ import (
 	"github.com/dotenx/dotenx/ao-api/models"
 )
 
-// TODO refactor this file
-
 func (cm *crudManager) CreatePipeLine(base *models.Pipeline, pipeline *models.PipelineVersion, isTemplate bool, isInteraction bool) (err error) {
-	// checking if all integrations in an interaction or template have provider
-	for _, task := range pipeline.Manifest.Tasks {
-		if task.Integration != "" && (isInteraction || isTemplate) {
-			integration, err := cm.IntegrationService.GetIntegrationByName(base.AccountId, task.Integration)
-			if err != nil {
-				return err
-			}
-			if integration.Provider == "" {
-				return errors.New("your integrations must have provider")
-			}
-		}
-		if isInteraction {
-			body := task.Body.(models.TaskBodyMap)
-			for key, value := range body {
-				if fmt.Sprintf("%v", value) == "" {
-					val := insertDto{
-						Source: config.Configs.App.InteractionBodyKey,
-						Key:    key,
-					}
-					body[key] = val
-				}
-			}
-		}
-	}
-	for _, trigger := range pipeline.Manifest.Triggers {
-		if trigger.Integration != "" && (isInteraction || isTemplate) {
-			integration, err := cm.IntegrationService.GetIntegrationByName(base.AccountId, trigger.Integration)
-			if err != nil {
-				return err
-			}
-			if integration.Provider == "" {
-				if isInteraction || isTemplate {
-					return errors.New("your integrations must have provider")
-				}
-			}
-		}
+	pipeline.Manifest.Tasks, err = cm.prepareTasks(pipeline.Manifest.Tasks, base.AccountId, isTemplate, isInteraction)
+	if err != nil {
+		return err
 	}
 	err = cm.Store.Create(noContext, base, pipeline, isTemplate, isInteraction)
 	if err != nil {
@@ -57,96 +22,47 @@ func (cm *crudManager) CreatePipeLine(base *models.Pipeline, pipeline *models.Pi
 	if err != nil {
 		return
 	}
-	triggers := make([]*models.EventTrigger, 0)
-	for _, tr := range pipeline.Manifest.Triggers {
-		tr.Endpoint = p.Endpoint
-		tr.Pipeline = base.Name
-		triggers = append(triggers, &models.EventTrigger{
-			Name:        tr.Name,
-			AccountId:   tr.AccountId,
-			Type:        tr.Type,
-			Endpoint:    p.Endpoint,
-			Pipeline:    base.Name,
-			Integration: tr.Integration,
-			Credentials: tr.Credentials,
-		})
+	triggers, err := cm.getTriggersArray(pipeline.Manifest.Triggers, base.Name, p.Endpoint, base.AccountId, p.IsTemplate)
+	if err != nil {
+		return
 	}
 	return cm.TriggerService.AddTriggers(base.AccountId, triggers, p.Endpoint)
 }
 
 func (cm *crudManager) UpdatePipeline(base *models.Pipeline, pipeline *models.PipelineVersion) error {
 	p, err := cm.Store.GetByName(noContext, base.AccountId, base.Name)
-	if err == nil && p.PipelineDetailes.Id != "" {
-		err := cm.DeletePipeline(base.AccountId, base.Name, true)
-		if err != nil {
-			return errors.New("error in deleting old version: " + err.Error())
-		}
-		for _, task := range pipeline.Manifest.Tasks {
-			if task.Integration != "" && (p.IsInteraction || p.IsTemplate) {
-				integration, err := cm.IntegrationService.GetIntegrationByName(base.AccountId, task.Integration)
-				if err != nil {
-					return err
-				}
-				if integration.Provider == "" {
-					return errors.New("your integrations must have provider")
-				}
-			}
-			if p.IsInteraction {
-				body := task.Body.(models.TaskBodyMap)
-				for key, value := range body {
-					if fmt.Sprintf("%v", value) == "" {
-						val := insertDto{
-							Source: config.Configs.App.InteractionBodyKey,
-							Key:    key,
-						}
-						body[key] = val
-					}
-				}
-			}
-		}
-		for _, trigger := range pipeline.Manifest.Triggers {
-			if trigger.Integration != "" && p.IsTemplate {
-				integration, err := cm.IntegrationService.GetIntegrationByName(base.AccountId, trigger.Integration)
-				if err != nil {
-					return err
-				}
-				if integration.Provider == "" {
-					return errors.New("your integrations must have provider")
-				}
-			}
-		}
-		err = cm.Store.Create(noContext, base, pipeline, p.IsTemplate, p.IsInteraction)
-		if err != nil {
-			return errors.New("error in creating new version: " + err.Error())
-		}
-		newP, err := cm.GetPipelineByName(base.AccountId, base.Name)
+	if err != nil || p.PipelineDetailes.Id == "" {
+		return errors.New("your Automation has not been saved yet")
+	}
+	err = cm.DeletePipeline(base.AccountId, base.Name, true)
+	if err != nil {
+		return errors.New("error in deleting old version: " + err.Error())
+	}
+	pipeline.Manifest.Tasks, err = cm.prepareTasks(pipeline.Manifest.Tasks, base.AccountId, p.IsTemplate, p.IsInteraction)
+	if err != nil {
+		return err
+	}
+	err = cm.Store.Create(noContext, base, pipeline, p.IsTemplate, p.IsInteraction)
+	if err != nil {
+		return errors.New("error in creating new version: " + err.Error())
+	}
+	newP, err := cm.GetPipelineByName(base.AccountId, base.Name)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if p.IsActive {
+		err := cm.ActivatePipeline(base.AccountId, newP.PipelineDetailes.Id)
 		if err != nil {
 			log.Println(err)
 			return err
 		}
-		if p.IsActive {
-			err := cm.ActivatePipeline(base.AccountId, newP.PipelineDetailes.Id)
-			if err != nil {
-				log.Println(err)
-				return err
-			}
-		}
-		triggers := make([]*models.EventTrigger, 0)
-		for _, tr := range pipeline.Manifest.Triggers {
-			triggers = append(triggers, &models.EventTrigger{
-				Name:        tr.Name,
-				AccountId:   tr.AccountId,
-				Type:        tr.Type,
-				Endpoint:    tr.Endpoint,
-				Pipeline:    tr.Pipeline,
-				Integration: tr.Integration,
-				Credentials: tr.Credentials,
-			})
-		}
-		return cm.TriggerService.UpdateTriggers(base.AccountId, triggers, newP.Endpoint)
-	} else {
-		return errors.New("your Automation has not been saved yet")
 	}
+	triggers, err := cm.getTriggersArray(pipeline.Manifest.Triggers, base.Name, newP.Endpoint, base.AccountId, newP.IsTemplate)
+	if err != nil {
+		return err
+	}
+	return cm.TriggerService.UpdateTriggers(base.AccountId, triggers, newP.Endpoint)
 }
 
 func (cm *crudManager) GetPipelineByName(accountId string, name string) (models.PipelineSummery, error) {
@@ -200,4 +116,61 @@ type automationDto struct {
 type insertDto struct {
 	Source string `json:"source"`
 	Key    string `json:"key"`
+}
+
+// checks trigger integration for templates and also convert trigger map to array of triggers
+func (cm *crudManager) getTriggersArray(triggers map[string]models.EventTrigger, pipelineName, endpoint, accountId string, isTemplate bool) ([]*models.EventTrigger, error) {
+	arr := make([]*models.EventTrigger, 0)
+	for _, tr := range triggers {
+		// check integration provider if it is a template
+		if tr.Integration != "" && isTemplate {
+			integration, err := cm.IntegrationService.GetIntegrationByName(accountId, tr.Integration)
+			if err != nil {
+				return nil, err
+			}
+			if integration.Provider == "" {
+				return nil, errors.New("your integrations must have provider")
+
+			}
+		}
+		arr = append(arr, &models.EventTrigger{
+			Name:        tr.Name,
+			AccountId:   tr.AccountId,
+			Type:        tr.Type,
+			Endpoint:    endpoint,
+			Pipeline:    pipelineName,
+			Integration: tr.Integration,
+			Credentials: tr.Credentials,
+		})
+	}
+	return arr, nil
+}
+
+// checks tasks integration for templates and also change empty task field values to get value from run time data for interactions
+func (cm *crudManager) prepareTasks(tasks map[string]models.Task, accountId string, isTemplate, isInteraction bool) (map[string]models.Task, error) {
+	//preparedTasks := make(map[string]models.Task)
+	for _, task := range tasks {
+		if task.Integration != "" && (isInteraction || isTemplate) {
+			integration, err := cm.IntegrationService.GetIntegrationByName(accountId, task.Integration)
+			if err != nil {
+				return nil, err
+			}
+			if integration.Provider == "" {
+				return nil, errors.New("your integrations must have provider")
+			}
+		}
+		if isInteraction {
+			body := task.Body.(models.TaskBodyMap)
+			for key, value := range body {
+				if fmt.Sprintf("%v", value) == "" {
+					val := insertDto{
+						Source: config.Configs.App.InteractionBodyKey,
+						Key:    key,
+					}
+					body[key] = val
+				}
+			}
+		}
+	}
+	return tasks, nil
 }
