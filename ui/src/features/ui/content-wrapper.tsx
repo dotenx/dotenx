d3bd0e82e8ae16ff1674482577af30c457dce67d
frustak <soulburn007@gmail.com>
2022-07-05 10:59:32 +0430

    Fix sidebar

--- ui/src/features/ui/content-wrapper.tsx
+++ ui/src/features/ui/content-wrapper.tsx
@@ -7,5 +7,5 @@ interface ContentWrapperProps {
 }
 
 export function ContentWrapper({ children, className }: ContentWrapperProps) {
-	return <main className={clsx('px-32 py-16 space-y-10 grow', className)}>{children}</main>
+	return <main className={clsx('pr-32 pl-52 py-16 space-y-10 grow', className)}>{children}</main>
 }
