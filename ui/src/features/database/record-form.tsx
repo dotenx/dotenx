e07e5af4f4ffb830c8e1f3bea33ed67e5485dc33
frustak <soulburn007@gmail.com>
2022-07-05 13:16:27 +0430

    Implement edit record form

--- ui/src/features/database/record-form.tsx
+++ ui/src/features/database/record-form.tsx
@@ -1,6 +1,13 @@
 import { useForm } from 'react-hook-form'
 import { useMutation, useQueryClient } from 'react-query'
-import { addRecord, AddRecordRequest, QueryKey } from '../../api'
+import {
+	addRecord,
+	AddRecordRequest,
+	QueryKey,
+	TableRecord,
+	updateRecord,
+	UpdateRecordRequest,
+} from '../../api'
 import { useModal } from '../hooks'
 import { Button, Field, Form } from '../ui'
 
@@ -36,6 +43,47 @@ export function RecordForm({
 			</div>
 			<Button type="submit" loading={mutation.isLoading}>
 				Add Record
+			</Button>
+		</Form>
+	)
+}
+
+export function EditRecordForm({
+	columns,
+	projectTag,
+	tableName,
+	rowId,
+	defaultValues,
+}: {
+	columns: string[]
+	projectTag: string
+	tableName: string
+	rowId: string
+	defaultValues: TableRecord
+}) {
+	const form = useForm({ defaultValues })
+	const client = useQueryClient()
+	const modal = useModal()
+	const mutation = useMutation(
+		(payload: UpdateRecordRequest) => updateRecord(projectTag, tableName, rowId, payload),
+		{
+			onSuccess: () => {
+				client.invalidateQueries(QueryKey.GetTableRecords)
+				modal.close()
+			},
+		}
+	)
+	const onSubmit = form.handleSubmit((values) => mutation.mutate(values))
+
+	return (
+		<Form className="h-full" onSubmit={onSubmit}>
+			<div className="space-y-5 grow">
+				{columns.map((column) => (
+					<Field key={column} label={column} name={column} control={form.control} />
+				))}
+			</div>
+			<Button type="submit" loading={mutation.isLoading}>
+				Update Record
 			</Button>
 		</Form>
 	)
