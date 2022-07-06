337e50f5806402e0f8d6647c382a9d1aa6fe9a87
frustak <soulburn007@gmail.com>
2022-07-06 12:15:51 +0430

    Improve add new table modal

--- ui/src/pages/tables.tsx
+++ ui/src/pages/tables.tsx
@@ -1,7 +1,7 @@
 import { Navigate, useParams } from 'react-router-dom'
 import { TableForm, TableList } from '../features/database'
 import { Modals } from '../features/hooks'
-import { ContentWrapper, Modal } from '../features/ui'
+import { ContentWrapper, NewModal } from '../features/ui'
 
 export default function TablesPage() {
 	const { projectName } = useParams()
@@ -15,9 +15,9 @@ function Tables({ name }: { name: string }) {
 			<ContentWrapper>
 				<TableList projectName={name} />
 			</ContentWrapper>
-			<Modal kind={Modals.NewTable} fluid title="New Table">
+			<NewModal kind={Modals.NewTable} title="Add a new table">
 				<TableForm projectName={name} />
-			</Modal>
+			</NewModal>
 		</>
 	)
 }
