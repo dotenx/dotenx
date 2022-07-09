cb1d9f612d2f400f78fb070afd292c82d2ee1347
frustak <soulburn007@gmail.com>
2022-07-09 13:38:47 +0430

    improve back to project link

--- ui/src/features/ui/sidebar.tsx
+++ ui/src/features/ui/sidebar.tsx
@@ -9,7 +9,7 @@ import {
 	BsWindowSidebar,
 } from 'react-icons/bs'
 import { FaUsers } from 'react-icons/fa'
-import { IoArrowForward, IoExit } from 'react-icons/io5'
+import { IoArrowBack, IoExit } from 'react-icons/io5'
 import { useMutation } from 'react-query'
 import { useMatch, useParams } from 'react-router-dom'
 import { logout } from '../../api/admin'
@@ -67,17 +67,17 @@ export const Sidebar = memo(() => {
 			{projectName && (
 				<div className="px-5 mt-10 text-slate-700" title="Back to Projects">
 					<a
-						className="px-3.5 py-1 font-medium transition bg-white rounded hover:bg-rose-50 flex items-center justify-between"
+						className="flex items-center gap-2 px-2.5 py-1 font-medium transition bg-white rounded hover:bg-rose-50"
 						href="https://admin.dotenx.com/projects"
 					>
+						<div className="w-0 transition-all scale-0 opacity-0 group-hover:opacity-100 group-hover:w-6 group-hover:scale-100">
+							<IoArrowBack />
+						</div>
 						<div>
 							<span className="text-center capitalize">{projectName[0]}</span>
 							<span className="transition opacity-0 group-hover:opacity-100">
 								{projectName.substring(1)}
 							</span>
-						</div>
-						<div className="transition opacity-0 group-hover:opacity-100">
-							<IoArrowForward />
 						</div>
 					</a>
 				</div>
