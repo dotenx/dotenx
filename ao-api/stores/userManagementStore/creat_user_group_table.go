1f5bbf77d06e77c005a4a2c3d240e6b4a42b55ee
Armin <armin.godarzi@utopiops.com>
2022-07-14 17:18:44 +0430

    seed to add 4 default user group added

--- ao-api/stores/userManagementStore/creat_user_group_table.go
+++ ao-api/stores/userManagementStore/creat_user_group_table.go
@@ -18,6 +18,22 @@ func (store *userManagementStore) CreateUserGroupTable(db *dbPkg.DB) (err error)
 	if err != nil {
 		return err
 	}
+	_, err = db.Connection.Exec(inserUsersUserGroup)
+	if err != nil {
+		return err
+	}
+	_, err = db.Connection.Exec(insertReadersUserGroup)
+	if err != nil {
+		return err
+	}
+	_, err = db.Connection.Exec(insertEditorsUserGroup)
+	if err != nil {
+		return err
+	}
+	_, err = db.Connection.Exec(insertWritersUserGroup)
+	if err != nil {
+		return err
+	}
 	return
 }
 
@@ -29,4 +45,34 @@ delete_list                JSONB,
 update_list                JSONB,
 select_list                JSONB
 )
+`
+var inserUsersUserGroup = `
+INSERT INTO user_group (name, insert_list, delete_list, update_list, select_list)
+VALUES ('users', null, null, null, null)
+`
+
+var insertWritersUserGroup = `
+INSERT INTO user_group (name, insert_list, delete_list, update_list, select_list)
+VALUES ('writers',null, ' {
+        "*": "*"
+    }', ' {
+        "*": "*"
+    }', ' {
+        "*": "*"
+    }')
+`
+
+var insertReadersUserGroup = `
+INSERT INTO user_group (name, insert_list, delete_list, update_list, select_list)
+VALUES ('readers', null, null, null, ' {
+        "*": "*"
+    }')
+`
+var insertEditorsUserGroup = `
+INSERT INTO user_group (name, insert_list, delete_list, update_list, select_list)
+VALUES ('editors', null, null, ' {
+        "*": "*"
+    }', ' {
+        "*": "*"
+    }')
 `
