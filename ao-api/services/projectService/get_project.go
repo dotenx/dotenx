bf59fd5239f64028c63e79b0336cff1eb90053d4
Armin <armin.godarzi@utopiops.com>
2022-07-23 18:13:34 +0430

    default user group added to get project response

--- ao-api/services/projectService/get_project.go
+++ ao-api/services/projectService/get_project.go
@@ -3,11 +3,25 @@ package projectService
 import (
 	"context"
 
+	"github.com/dotenx/dotenx/ao-api/db/dbutil"
 	"github.com/dotenx/dotenx/ao-api/models"
 )
 
 func (ps *projectService) GetProject(accountId string, projectName string) (models.Project, error) {
 	noContext := context.Background()
-
-	return ps.Store.GetProject(noContext, accountId, projectName)
+	project, err := ps.Store.GetProject(noContext, accountId, projectName)
+	if err != nil {
+		return models.Project{}, err
+	}
+	db, closeFunc, err := dbutil.GetDbInstance(project.AccountId, project.Name)
+	if err != nil {
+		return models.Project{}, err
+	}
+	defer closeFunc(db.Connection)
+	user_group, err := ps.TpUserStore.GetDefaultUserGroup(db)
+	if err != nil {
+		return models.Project{}, err
+	}
+	project.DefaultUserGroup = user_group.Name
+	return project, nil
 }
--- ao-api/services/projectService/get_project_by_tag.go
+++ ao-api/services/projectService/get_project_by_tag.go
@@ -3,11 +3,26 @@ package projectService
 import (
 	"context"
 
+	"github.com/dotenx/dotenx/ao-api/db/dbutil"
 	"github.com/dotenx/dotenx/ao-api/models"
 )
 
 func (ps *projectService) GetProjectByTag(tag string) (models.Project, error) {
 	noContext := context.Background()
 
-	return ps.Store.GetProjectByTag(noContext, tag)
+	project, err := ps.Store.GetProjectByTag(noContext, tag)
+	if err != nil {
+		return models.Project{}, err
+	}
+	db, closeFunc, err := dbutil.GetDbInstance(project.AccountId, project.Name)
+	if err != nil {
+		return models.Project{}, err
+	}
+	defer closeFunc(db.Connection)
+	user_group, err := ps.TpUserStore.GetDefaultUserGroup(db)
+	if err != nil {
+		return models.Project{}, err
+	}
+	project.DefaultUserGroup = user_group.Name
+	return project, nil
 }
