5389ed521245d4af98671823a2e5d2250a29a88d
Armin <armin.godarzi@utopiops.com>
2022-07-24 09:59:59 +0430

    changing set user group flow

--- ao-api/controllers/userManagement/set_user_group.go
+++ ao-api/controllers/userManagement/set_user_group.go
@@ -1,6 +1,7 @@
 package userManagement
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
@@ -12,37 +13,46 @@ type setReq struct {
 }
 
 func (umc *UserManagementController) SetUserGroup() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
 		var body setReq
-		projectTag := ctx.Param("tag")
-		ugName := ctx.Param("name")
-		if ctx.ShouldBindJSON(&body) != nil || body.TpAccountId == "" || projectTag == "" || ugName == "" {
+		projectTag := c.Param("tag")
+		ugName := c.Param("name")
+		if c.ShouldBindJSON(&body) != nil || body.TpAccountId == "" || projectTag == "" || ugName == "" {
 			log.Println("body:", body)
 			log.Println("projectTag:", projectTag)
 			log.Println("ugName:", ugName)
-			ctx.Status(http.StatusBadRequest)
+			c.Status(http.StatusBadRequest)
 			return
 		}
 		user, err := umc.Service.GetUserInfoById(body.TpAccountId, projectTag)
 
 		if err != nil {
 			if err.Error() == "user not found" {
-				ctx.JSON(http.StatusNotFound, gin.H{
+				c.JSON(http.StatusNotFound, gin.H{
 					"message": "accountId is incorrect",
 				})
 				return
 			}
-			ctx.Status(http.StatusInternalServerError)
+			c.Status(http.StatusInternalServerError)
 			return
 		}
 		user.UserGroup = ugName
-
-		err = umc.Service.UpdateUserGroup(*user, projectTag)
-		if err != nil {
-			log.Println(err.Error())
-			ctx.Status(http.StatusInternalServerError)
+		tokenType, exist := c.Get("tokenType")
+		if !exist {
+			err := errors.New("missing type field of token")
+			c.AbortWithError(http.StatusForbidden, err)
 			return
 		}
-		ctx.Status(http.StatusOK)
+		if tokenType != "tp" {
+			err = umc.Service.UpdateUserGroup(*user, projectTag)
+			if err != nil {
+				log.Println(err.Error())
+				c.Status(http.StatusInternalServerError)
+				return
+			}
+			c.Status(http.StatusOK)
+			return
+		}
+		// TODO run query to update user info table
 	}
 }
