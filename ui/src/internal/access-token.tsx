a635f9a40ed7460136619f91319d9fc380b9554d
Erfan QasemiZade <erfan.qasemizade@utopiops.com>
2022-07-25 07:36:45 +0000

    DTX-79: Add personal access token feature to user management

---
+++ ui/src/internal/access-token.tsx
@@ -0,0 +1,83 @@
+import { ActionIcon, Button, Code } from '@mantine/core'
+import { useClipboard } from '@mantine/hooks'
+import { IoCheckmark, IoCopy, IoRepeat, IoTrash } from 'react-icons/io5'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { Loader } from '../features/ui'
+import {
+	deleteAccessToken,
+	getAccessToken,
+	InternalQueryKey,
+	setAccessToken,
+	updateAccessToken,
+} from './internal-api'
+
+export function AccessToken() {
+	const queryClient = useQueryClient()
+	const query = useQuery(InternalQueryKey.GetAccessToken, getAccessToken)
+	const generateMutation = useMutation(setAccessToken, {
+		onSuccess: () => queryClient.invalidateQueries(InternalQueryKey.GetAccessToken),
+	})
+	const regenerateMutation = useMutation(updateAccessToken, {
+		onSuccess: () => queryClient.invalidateQueries(InternalQueryKey.GetAccessToken),
+	})
+	const deleteMutation = useMutation(deleteAccessToken, {
+		onSuccess: () => {
+			queryClient.invalidateQueries(InternalQueryKey.GetAccessToken)
+			query.remove()
+		},
+	})
+	const accessToken = query.data?.data.accessToken
+
+	if (query.isLoading) return <Loader />
+
+	if (!accessToken) {
+		return (
+			<Button
+				type="button"
+				onClick={() => generateMutation.mutate()}
+				loading={generateMutation.isLoading}
+			>
+				Generate personal access token
+			</Button>
+		)
+	}
+
+	return (
+		<div>
+			<div className="flex items-center justify-between gap-2">
+				{accessToken && <Code>{accessToken}</Code>}
+				<div className="flex gap-0.5">
+					<ActionIcon
+						type="button"
+						title="Delete existing access token"
+						onClick={() => deleteMutation.mutate()}
+						loading={deleteMutation.isLoading}
+					>
+						<IoTrash />
+					</ActionIcon>
+					<ActionIcon
+						type="button"
+						title="Generate a new access token"
+						onClick={() => regenerateMutation.mutate()}
+						loading={regenerateMutation.isLoading}
+					>
+						<IoRepeat />
+					</ActionIcon>
+					<CopyButton text={accessToken} />
+				</div>
+			</div>
+			<p className="mt-10">Set this header in requests</p>
+			<Code>DTX-auth: {accessToken}</Code>
+		</div>
+	)
+}
+
+function CopyButton({ text }: { text: string }) {
+	const clipboard = useClipboard({ timeout: 1000 })
+
+	return (
+		<ActionIcon type="button" title="Copy access token" onClick={() => clipboard.copy(text)}>
+			{clipboard.copied ? <IoCheckmark /> : <IoCopy />}
+		</ActionIcon>
+	)
+}
