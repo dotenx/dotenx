9edea4d043cab8e3dc3815f787e3d89621142a2b
Utopiops <utopiops@gmail.com>
2022-10-04 11:02:01 +0000

    Dtx 242

---
+++ ui-builder/src/features/controllers/basic-components/base.ts
@@ -0,0 +1,11 @@
+export const elementBase = {
+	classNames: [],
+	bindings: {},
+	events: [],
+	id: '',
+	parentId: '',
+	repeatFrom: {
+		iterator: '',
+		name: '',
+	},
+}
--- ui-builder/src/features/controllers/index.ts
+++ ui-builder/src/features/controllers/index.ts
@@ -1,6 +1,11 @@
 import { Hero } from './hero'
+import { SignInBasic } from './sign-in-basic'
+import { SignUpBasic } from './sign-up-basic'
+
+// import { Test } from './test2'
 
 export const controllers = [
+	{ title: 'Sign In/Up', items: [SignInBasic, SignUpBasic] },
 	{ title: 'Base', items: [Hero] },
 	{ title: 'Others', items: [Hero] },
 ] as const
