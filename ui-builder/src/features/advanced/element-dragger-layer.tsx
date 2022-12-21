a5d320f21bd966358b69618d18b38e78d2abcd55
frustak <soulburn007@gmail.com>
2022-12-21 12:35:27 +0330

    implement plugin tab

--- ui-builder/src/features/advanced/element-dragger-layer.tsx
+++ ui-builder/src/features/advanced/element-dragger-layer.tsx
@@ -1,9 +1,12 @@
-import { Divider, Tabs } from '@mantine/core'
+import { Divider, Loader, Tabs } from '@mantine/core'
+import { useQuery } from '@tanstack/react-query'
 import { atom, useAtom } from 'jotai'
-import { TbComponents, TbLayersDifference } from 'react-icons/tb'
+import { TbComponents, TbLayersDifference, TbPlug, TbPuzzle } from 'react-icons/tb'
+import { QueryKey } from '../../api'
 import { ComponentDragger } from '../marketplace/component-dragger'
 import { DesignSystems } from '../marketplace/design-systems'
-import { ElementDragger } from './element-dragger'
+import { getPlugins } from '../plugins/api'
+import { ElementCard, ElementDragger } from './element-dragger'
 import { DndLayers } from './layers'
 
 export const sidebarAtom = atom<{ tab: string | null }>({ tab: 'elements' })
@@ -18,12 +21,9 @@ export function ElementDraggerAndLayers() {
 			onTabChange={(value) => setSidebar({ tab: value })}
 		>
 			<Tabs.List grow>
-				<Tabs.Tab value="elements" icon={<TbComponents size={14} />}>
-					Elements
-				</Tabs.Tab>
-				<Tabs.Tab value="layers" icon={<TbLayersDifference size={14} />}>
-					Layers
-				</Tabs.Tab>
+				<Tabs.Tab value="elements" icon={<TbComponents size={16} />} title="Elements" />
+				<Tabs.Tab value="layers" icon={<TbLayersDifference size={16} />} title="Layers" />
+				<Tabs.Tab value="plugins" icon={<TbPlug size={16} />} title="Layers" />
 			</Tabs.List>
 
 			<Tabs.Panel value="elements" pt="xs">
@@ -31,6 +31,9 @@ export function ElementDraggerAndLayers() {
 			</Tabs.Panel>
 			<Tabs.Panel value="layers" pt="xs">
 				<DndLayers />
+			</Tabs.Panel>
+			<Tabs.Panel value="plugins" pt="xs">
+				<PluginsTab />
 			</Tabs.Panel>
 		</Tabs>
 	)
@@ -43,6 +46,21 @@ function ElementDraggerTab() {
 			<Divider mt="xl" mb="xs" label="Components" labelPosition="center" />
 			<ComponentDragger />
 			<DesignSystems />
+		</div>
+	)
+}
+
+function PluginsTab() {
+	const pluginsQuery = useQuery([QueryKey.Plugins], getPlugins)
+	const plugins = pluginsQuery.data?.data ?? []
+
+	if (pluginsQuery.isLoading) return <Loader size="xs" mx="auto" />
+
+	return (
+		<div className="grid grid-cols-2 gap-2">
+			{plugins.map((plugin) => (
+				<ElementCard key={plugin.id} label={plugin.name} icon={<TbPuzzle />} />
+			))}
 		</div>
 	)
 }
