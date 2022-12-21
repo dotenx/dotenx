4711108e7f3ff03fb7ffad51d7684465abf37289
frustak <soulburn007@gmail.com>
2022-12-21 13:04:26 +0330

    improve plugin tab

--- ui-builder/src/features/advanced/element-dragger-layer.tsx
+++ ui-builder/src/features/advanced/element-dragger-layer.tsx
@@ -1,8 +1,11 @@
-import { Divider, Loader, Tabs } from '@mantine/core'
+import { Anchor, Button, Divider, Loader, Tabs } from '@mantine/core'
 import { useQuery } from '@tanstack/react-query'
-import { atom, useAtom } from 'jotai'
+import { atom, useAtom, useSetAtom } from 'jotai'
 import { TbComponents, TbLayersDifference, TbPlug, TbPuzzle } from 'react-icons/tb'
+import { Link } from 'react-router-dom'
 import { QueryKey } from '../../api'
+import { Draggable, DraggableMode } from '../dnd/draggable'
+import { PluginElement } from '../elements/extensions/plugin'
 import { ComponentDragger } from '../marketplace/component-dragger'
 import { DesignSystems } from '../marketplace/design-systems'
 import { getPlugins } from '../plugins/api'
@@ -51,16 +54,34 @@ function ElementDraggerTab() {
 }
 
 function PluginsTab() {
+	const setSidebar = useSetAtom(sidebarAtom)
 	const pluginsQuery = useQuery([QueryKey.Plugins], getPlugins)
 	const plugins = pluginsQuery.data?.data ?? []
 
 	if (pluginsQuery.isLoading) return <Loader size="xs" mx="auto" />
+	if (plugins.length === 0) return <p className="text-xs text-center">No plugins found</p>
 
 	return (
-		<div className="grid grid-cols-2 gap-2">
-			{plugins.map((plugin) => (
-				<ElementCard key={plugin.id} label={plugin.name} icon={<TbPuzzle />} />
-			))}
+		<div>
+			<Anchor component={Link} to="/plugins">
+				<Button size="xs" fullWidth>
+					Go to plugins
+				</Button>
+			</Anchor>
+			<div className="grid grid-cols-2 gap-2 mt-4">
+				{plugins.map((plugin) => (
+					<Draggable
+						key={plugin.id}
+						data={{
+							mode: DraggableMode.AddWithData,
+							data: PluginElement.create(plugin),
+						}}
+						onDrag={() => setSidebar({ tab: 'layers' })}
+					>
+						<ElementCard label={plugin.name} icon={<TbPuzzle />} />
+					</Draggable>
+				))}
+			</div>
 		</div>
 	)
 }
