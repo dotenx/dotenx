b01939467e859e6e856874c1d26200b25f83a51c
Hojjat <hojjat.hajizadeh@utopiops.com>
2022-12-26 21:54:53 +0330

    Add an endpoint to return publish and preview url and its dependencies

--- ao-api/models/uipage_model.go
+++ ao-api/models/uipage_model.go
@@ -2,14 +2,17 @@ package models
 
 import (
 	"encoding/json"
+	"time"
 )
 
 type UIPage struct {
-	Name       string          `json:"name" db:"name"`
-	AccountId  string          `db:"account_id" json:"-"`
-	ProjectTag string          `db:"project_tag" json:"project_tag"`
-	Content    json.RawMessage `db:"content" json:"content"`
-	Status     string          `db:"status" json:"status"`
+	Name                   string          `json:"name" db:"name"`
+	AccountId              string          `db:"account_id" json:"-"`
+	ProjectTag             string          `db:"project_tag" json:"project_tag"`
+	Content                json.RawMessage `db:"content" json:"content"`
+	Status                 string          `db:"status" json:"status"`
+	LastPublishedAt        time.Time       `db:"last_published_at" json:"last_published_at"`
+	LastPreviewPublishedAt time.Time       `db:"last_preview_published_at" json:"last_preview_published_at"`
 }
 
 type UIComponent struct {
