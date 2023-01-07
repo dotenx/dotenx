98ed86fcb741692e63f8c6b6795257a8b8aac6e7
frustak <soulburn007@gmail.com>
2023-01-07 09:57:18 +0330

    refactor animation schema

--- ui-builder/src/features/animations/schema.ts
+++ ui-builder/src/features/animations/schema.ts
@@ -1,23 +1,23 @@
 import { z } from 'zod'
 import { DIRECTIONS, EASINGS } from './options'
 
+const keyframe = z.object({
+	id: z.string(),
+	value: z.string(),
+	duration: z.number().optional(),
+	delay: z.number().optional(),
+})
+
+const propertyOptions = z.object({
+	id: z.string(),
+	name: z.string(),
+	keyframes: z.array(keyframe),
+})
+
 export const animationSchema = z.object({
 	id: z.string(),
 	name: z.string().min(1).max(50),
-	properties: z.array(
-		z.object({
-			id: z.string(),
-			name: z.string(),
-			keyframes: z.array(
-				z.object({
-					id: z.string(),
-					value: z.string(),
-					duration: z.number().optional(),
-					delay: z.number().optional(),
-				})
-			),
-		})
-	),
+	properties: z.array(propertyOptions),
 	duration: z.number(),
 	delay: z.number(),
 	easing: z.enum(EASINGS),
@@ -28,3 +28,5 @@ export const animationSchema = z.object({
 })
 
 export type Animation = z.infer<typeof animationSchema>
+export type PropertyOptions = z.infer<typeof propertyOptions>
+export type Keyframe = z.infer<typeof keyframe>
