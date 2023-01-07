b2231cca30dec260e8b6185280f796979275c58c
frustak <soulburn007@gmail.com>
2023-01-07 12:09:15 +0330

    add more presets

--- ui-builder/src/features/animations/presets.ts
+++ ui-builder/src/features/animations/presets.ts
@@ -117,6 +117,18 @@ export const PRESETS: Animation[] = [
 					},
 				],
 			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
 		],
 		duration: 1000,
 		delay: 0,
@@ -174,6 +186,579 @@ export const PRESETS: Animation[] = [
 					{
 						id: uuid(),
 						value: '180deg',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-center',
+		name: 'Scale Up Center',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-bl',
+		name: 'Scale Up Bottom Left',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-ver-center',
+		name: 'Scale Up Vertical Center',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scaleY',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-top',
+		name: 'Scale Up Top',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-left',
+		name: 'Scale Up Left',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-ver-top',
+		name: 'Scale Up Vertical Top',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scaleY',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-tr',
+		name: 'Scale Up Top Right',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-tl',
+		name: 'Scale Up Top Left',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-tl',
+		name: 'Scale Up Top Left',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-ver-bottom',
+		name: 'Scale Up Vertical Bottom',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scaleY',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-right',
+		name: 'Scale Up Right',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scale',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+			{
+				id: uuid(),
+				name: 'transformOrigin',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '50% 50%',
+						duration: 0,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-hor-center',
+		name: 'Scale Up Horizontal Center',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scaleX',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-hor-left',
+		name: 'Scale Up Horizontal Left',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scaleX',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-bottom',
+		name: 'Scale Up Bottom',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scaleY',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
+						duration: 1000,
+						delay: 0,
+					},
+				],
+			},
+		],
+		duration: 1000,
+		delay: 0,
+		easing: 'linear',
+		easingParams: [],
+		direction: 'normal',
+		loop: false,
+		stagger: false,
+	},
+	{
+		id: 'scale-up-hor-right',
+		name: 'Scale Up Horizontal Right',
+		properties: [
+			{
+				id: uuid(),
+				name: 'scaleX',
+				keyframes: [
+					{
+						id: uuid(),
+						value: '0',
+						duration: 0,
+						delay: 0,
+					},
+					{
+						id: uuid(),
+						value: '1',
 						duration: 1000,
 						delay: 0,
 					},
