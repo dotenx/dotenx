8390496f00cdffef441fa8cc2b3d344119b81623
frustak <soulburn007@gmail.com>
2023-01-08 14:47:30 +0330

    refactor text element input to use input styler

--- ui-builder/src/features/ui/text-element-input.tsx
+++ ui-builder/src/features/ui/text-element-input.tsx
@@ -1,12 +1,10 @@
-import { ActionIcon, CloseButton, Popover, ScrollArea, TextInput } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks'
-import { TbDroplet } from 'react-icons/tb'
-import { Element } from '../elements/element'
+import { TextInput } from '@mantine/core'
 import { useSetWithElement } from '../elements/elements-store'
 import { TextElement } from '../elements/extensions/text'
 import { Expression } from '../states/expression'
 import { SpacingEditor } from '../style/spacing-editor'
 import { TypographyEditor } from '../style/typography-editor'
+import { InputStyler } from './input-styler'
 
 export function TextElementInput({ label, element }: { label: string; element: TextElement }) {
 	const set = useSetWithElement(element)
@@ -21,43 +19,18 @@ export function TextElementInput({ label, element }: { label: string; element: T
 			name={label}
 			value={element.data.text.toString()}
 			onChange={(event) => setText(event.target.value)}
-			rightSection={<StyleSection element={element} />}
+			rightSection={<StyleEditor element={element} />}
 		/>
 	)
 }
 
-function StyleSection({ element }: { element: TextElement }) {
-	const [opened, openedHandlers] = useDisclosure(false)
-
+function StyleEditor({ element }: { element: TextElement }) {
 	return (
-		<Popover
-			shadow="md"
-			position="left"
-			withinPortal
-			width={300}
-			closeOnClickOutside={false}
-			opened={opened}
-		>
-			<Popover.Target>
-				<ActionIcon size="xs" onClick={openedHandlers.toggle}>
-					<TbDroplet size={12} />
-				</ActionIcon>
-			</Popover.Target>
-			<Popover.Dropdown sx={{ padding: 0 }}>
-				<CloseButton onClick={openedHandlers.close} size="xs" ml="auto" />
-				<ScrollArea sx={{ height: 400, padding: '0 16px 12px 16px' }} offsetScrollbars>
-					<StyleEditor element={element} />
-				</ScrollArea>
-			</Popover.Dropdown>
-		</Popover>
-	)
-}
-
-function StyleEditor({ element }: { element: Element }) {
-	return (
-		<div className="space-y-6 text-xs">
-			<TypographyEditor simple element={element} />
-			<SpacingEditor element={element} />
-		</div>
+		<InputStyler>
+			<div className="space-y-6 text-xs">
+				<TypographyEditor simple element={element} />
+				<SpacingEditor element={element} />
+			</div>
+		</InputStyler>
 	)
 }
