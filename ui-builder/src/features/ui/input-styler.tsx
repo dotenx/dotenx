d2b4654df1e9c288a7870fe5f59c731e03eb8201
frustak <soulburn007@gmail.com>
2023-01-22 12:50:01 +0330

    use drawer instead of popover in simple component style

--- ui-builder/src/features/ui/input-styler.tsx
+++ ui-builder/src/features/ui/input-styler.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, CloseButton, Popover, ScrollArea } from '@mantine/core'
+import { ActionIcon, Drawer } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { ReactNode } from 'react'
 import { TbDroplet } from 'react-icons/tb'
@@ -7,25 +7,20 @@ export function InputStyler({ children }: { children: ReactNode }) {
 	const [opened, openedHandlers] = useDisclosure(false)
 
 	return (
-		<Popover
-			shadow="md"
-			position="left"
-			withinPortal
-			width={300}
-			closeOnClickOutside={false}
-			opened={opened}
-		>
-			<Popover.Target>
-				<ActionIcon size="xs" onClick={openedHandlers.toggle}>
-					<TbDroplet size={12} />
-				</ActionIcon>
-			</Popover.Target>
-			<Popover.Dropdown sx={{ padding: 0 }}>
-				<CloseButton onClick={openedHandlers.close} size="xs" ml="auto" />
-				<ScrollArea sx={{ height: 400, padding: '0 16px 12px 16px' }} offsetScrollbars>
-					{children}
-				</ScrollArea>
-			</Popover.Dropdown>
-		</Popover>
+		<>
+			<ActionIcon size="xs" onClick={openedHandlers.toggle}>
+				<TbDroplet size={12} />
+			</ActionIcon>
+			<Drawer
+				opened={opened}
+				onClose={openedHandlers.close}
+				position="right"
+				padding="lg"
+				overlayOpacity={0}
+				size={310}
+			>
+				{children}
+			</Drawer>
+		</>
 	)
 }
