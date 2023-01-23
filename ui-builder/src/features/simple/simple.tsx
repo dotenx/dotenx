d45cc280955e6a594a3936314bad36ea96198897
frustak <soulburn007@gmail.com>
2023-01-23 12:32:50 +0330

    refactor simple app shell

--- ui-builder/src/features/simple/simple.tsx
+++ ui-builder/src/features/simple/simple.tsx
@@ -1,67 +1,41 @@
-import { AppShell, Aside, Header, Navbar, ScrollArea } from '@mantine/core'
-import { useClickOutside } from '@mantine/hooks'
-import { useAtomValue, useSetAtom } from 'jotai'
+import { AppShell } from '@mantine/core'
+import { useAtomValue } from 'jotai'
+import { CONTROLLERS } from '../controllers'
 import { previewAtom, TopBar } from '../page/top-bar'
+import { AppHeader } from '../ui/header'
+import { LeftSidebar } from '../ui/left-sidebar'
+import { RightSidebar } from '../ui/right-sidebar'
 import { SimpleLeftSidebar } from './left-sidebar'
 import { SimpleRightSidebar } from './right-sidebar'
-import { insertingAtom, SimpleCanvas } from './simple-canvas'
+import { SimpleCanvas } from './simple-canvas'
 
 export function Simple() {
 	const { isFullscreen } = useAtomValue(previewAtom)
-	const sidebars = isFullscreen ? {} : { navbar: <AppLeftSideBar />, aside: <AppRightSideBar /> }
+	const sidebars = isFullscreen ? {} : { navbar: <Navbar />, aside: <Aside /> }
 
 	return (
-		<AppShell header={<AppHeader />} padding={0} {...sidebars}>
+		<AppShell
+			header={
+				<AppHeader>
+					<TopBar />
+				</AppHeader>
+			}
+			padding={0}
+			{...sidebars}
+		>
 			<SimpleCanvas />
 		</AppShell>
 	)
 }
 
-function AppHeader() {
-	return (
-		<Header height={60} sx={{ zIndex: 110 }}>
-			<TopBar />
-		</Header>
-	)
-}
+const Navbar = () => (
+	<LeftSidebar>
+		<SimpleLeftSidebar components={CONTROLLERS} />
+	</LeftSidebar>
+)
 
-function AppLeftSideBar() {
-	const setInserting = useSetAtom(insertingAtom)
-	const outsideClickRef = useClickOutside(() => setInserting(null))
-
-	return (
-		<Navbar width={{ base: 310 }} ref={outsideClickRef}>
-			<Navbar.Section
-				component={ScrollArea}
-				grow
-				scrollbarSize={0}
-				scrollHideDelay={0}
-				offsetScrollbars
-			>
-				<div className="py-2 px-4">
-					<SimpleLeftSidebar />
-				</div>
-			</Navbar.Section>
-		</Navbar>
-	)
-}
-
-function AppRightSideBar() {
-	return (
-		<Aside width={{ base: 310 }}>
-			<Aside.Section
-				component={ScrollArea}
-				grow
-				scrollbarSize={4}
-				scrollHideDelay={0}
-				offsetScrollbars
-				mx="-xs"
-				px="xl"
-			>
-				<div className="py-2 px-1">
-					<SimpleRightSidebar />
-				</div>
-			</Aside.Section>
-		</Aside>
-	)
-}
+const Aside = () => (
+	<RightSidebar>
+		<SimpleRightSidebar />
+	</RightSidebar>
+)
