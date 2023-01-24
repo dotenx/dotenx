dcd6fda63ca4c17b42516f008e32f602804ecdde
frustak <soulburn007@gmail.com>
2023-01-24 12:26:36 +0330

    implement product list logic

--- ui-builder/src/features/ecommerce/components/product-list.tsx
+++ ui-builder/src/features/ecommerce/components/product-list.tsx
@@ -1,30 +1,65 @@
 import _ from 'lodash'
+import { API_URL } from '../../../api'
 import imageUrl from '../../../assets/components/about-left.png'
+import { uuid } from '../../../utils'
 import { gridCols } from '../../../utils/style-utils'
 import { Controller } from '../../controllers/controller'
 import { OptionsWrapper } from '../../controllers/helpers/options-wrapper'
+import { HttpMethod, useDataSourceStore } from '../../data-source/data-source-store'
+import { Element } from '../../elements/element'
+import { setElement } from '../../elements/elements-store'
 import { BoxElement } from '../../elements/extensions/box'
 import { ColumnsElement } from '../../elements/extensions/columns'
 import { ImageElement } from '../../elements/extensions/image'
 import { LinkElement } from '../../elements/extensions/link'
 import { TextElement } from '../../elements/extensions/text'
+import { useProjectStore } from '../../page/project-store'
 import { useSelectedElement } from '../../selection/use-selected-component'
 import { BoxStyler } from '../../simple/stylers/box-styler'
 import { ColumnsStyler } from '../../simple/stylers/columns-styler'
 import { TextStyler } from '../../simple/stylers/text-styler'
+import { Expression } from '../../states/expression'
 
 export class ProductList extends Controller {
 	name = 'Product list'
 	image = imageUrl
 	defaultData = component()
 	renderOptions = () => <ProductListOptions />
+
+	onCreate(root: Element) {
+		const projectTag = useProjectStore.getState().tag
+		const addDataSource = useDataSourceStore.getState().add
+		const id = uuid()
+		const dataSourceName = `product_list_${id}`
+		addDataSource({
+			id,
+			stateName: dataSourceName,
+			method: HttpMethod.Post,
+			url: Expression.fromString(
+				`${API_URL}/public/database/query/select/project/${projectTag}/table/products`
+			),
+			fetchOnload: true,
+			body: Expression.fromString(
+				JSON.stringify({ columns: ['id', 'name', 'price', 'image_url'] })
+			),
+			headers: '',
+			isPrivate: false,
+			properties: [],
+			onSuccess: [],
+		})
+		const column = root.find<BoxElement>(tagIds.column)!
+		setElement(
+			column,
+			(draft) => (draft.repeatFrom = { name: dataSourceName, iterator: 'product' })
+		)
+	}
 }
 
 function ProductListOptions() {
 	const component = useSelectedElement<BoxElement>()!
 	const title = component.find<TextElement>(tagIds.title)!
 	const grid = component.find<ColumnsElement>(tagIds.grid)!
-	const columns = component.findAll<ColumnsElement>(tagIds.column)!
+	const columns = component.findAll<BoxElement>(tagIds.column)!
 	const names = component.findAll<TextElement>(tagIds.name)!
 	const prices = component.findAll<TextElement>(tagIds.price)!
 
@@ -44,6 +79,7 @@ const tagIds = {
 	title: 'title',
 	grid: 'grid',
 	column: 'column',
+	image: 'image',
 	name: 'name',
 	price: 'price',
 }
@@ -87,12 +123,12 @@ function component() {
 }
 
 const column = () => {
-	const image = new ImageElement().css({
+	const image = new ImageElement().tag(tagIds.image).srcState('product.image_url').css({
 		flexGrow: '1',
 		backgroundColor: '#f5f5f5',
 	})
 
-	const name = new TextElement().tag(tagIds.name).text('Name').css({
+	const name = new TextElement().tag(tagIds.name).textState('product.name').css({
 		fontWeight: '500',
 	})
 
@@ -100,7 +136,7 @@ const column = () => {
 		textDecoration: 'none',
 	})
 
-	const price = new TextElement().tag(tagIds.price).text('$0.0').css({
+	const price = new TextElement().tag(tagIds.price).textState('product.price').css({
 		fontWeight: '500',
 	})
 
