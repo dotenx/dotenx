6609dd6eea49dc9ce48a958ba7da49db8bdfcb70
frustak <soulburn007@gmail.com>
2023-01-24 23:26:16 +0330

    implement product item

--- ui-builder/src/features/ecommerce/index.ts
+++ ui-builder/src/features/ecommerce/index.ts
@@ -1,9 +1,10 @@
 import { Components } from '../controllers'
+import { ProductItem } from './components/product-item'
 import { ProductList } from './components/product-list'
 
 export const ECOMMERCE_COMPONENTS: Components = [
 	{
 		title: 'E-commerce',
-		items: [ProductList],
+		items: [ProductList, ProductItem],
 	},
 ]
