312ebcbe2879507c54087c1d59d062066c9f18f6
frustak <soulburn007@gmail.com>
2023-01-24 09:58:48 +0330

    implement new box element input

--- ui-builder/src/features/ui/box-element-input.tsx
+++ ui-builder/src/features/ui/box-element-input.tsx
@@ -1,8 +1,12 @@
 import { ColorInput } from '@mantine/core'
 import { Element } from '../elements/element'
 import { useSetWithElement } from '../elements/elements-store'
+import { BackgroundsEditor } from '../style/background-editor'
+import { BordersEditor } from '../style/border-editor'
+import { SpacingEditor } from '../style/spacing-editor'
+import { InputStyler } from './input-styler'
 
-export function BoxElementInput({ element, label }: { element: Element; label: string }) {
+export function BoxElementInputSimple({ element, label }: { element: Element; label: string }) {
 	const set = useSetWithElement(element)
 	const setBgColor = (value: string) => {
 		set((draft) => (draft.style.desktop!.default!.backgroundColor = value))
@@ -16,5 +20,30 @@ export function BoxElementInput({ element, label }: { element: Element; label: s
 			size="xs"
 			format="hsla"
 		/>
+	)
+}
+
+export function BoxElementInput({
+	element,
+	label,
+}: {
+	element: Element | Element[]
+	label: string
+}) {
+	return (
+		<div className="flex justify-between items-center">
+			<p className="font-medium">{label}</p>
+			<StyleEditor element={element} />
+		</div>
+	)
+}
+
+function StyleEditor({ element }: { element: Element | Element[] }) {
+	return (
+		<InputStyler>
+			<BackgroundsEditor simple element={element} />
+			<BordersEditor simple element={element} />
+			<SpacingEditor element={element} />
+		</InputStyler>
 	)
 }
