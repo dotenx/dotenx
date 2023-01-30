6ab5ef94976d8b1c69e2058c96cb6f61be53e2a7
frustak <soulburn007@gmail.com>
2023-01-30 13:32:37 +0330

    implement hero section

--- ui-builder/src/features/ecommerce/sofa/theme.ts
+++ ui-builder/src/features/ecommerce/sofa/theme.ts
@@ -4,6 +4,7 @@ const theme = {
 	fontFamily: 'Inter',
 	colors: {
 		text: '#000000',
+		textLight: '#6F6F6F',
 		background: '#FFFFFF',
 		light: '#EEEEEE',
 		primary: '#F4A261',
@@ -12,66 +13,109 @@ const theme = {
 		secondary: '#2A9D8F',
 		secondaryDark: '#264653',
 	},
+	shadows: {
+		normal: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+	},
+	container: {
+		padding: '8.5rem',
+	},
 }
 
 const page = () =>
-	box()
-		.populate([navbar(), hero(), logos(), featuresText(), features(), info(), info(), cta()])
-		.css({
-			fontFamily: theme.fontFamily,
-		})
+	box([navbar(), hero(), logos(), featuresText(), features(), info(), info(), cta()]).css({
+		fontFamily: theme.fontFamily,
+		color: theme.colors.text,
+	})
 
 const navbar = () =>
-	box()
-		.populate([
-			box(),
-			box()
-				.populate([
-					txt('Category 1').unstyled(),
-					txt('Category 2').unstyled(),
-					txt('Case studies').unstyled(),
-				])
-				.css({
-					display: 'flex',
-					gap: '2.5rem',
-				}),
-			btn('Sign up').unstyled().css({
-				border: '1px solid',
-				borderRadius: '9999px',
-				padding: '1rem 3.5rem',
-				boxShadow: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-			}),
-		])
-		.css({
-			fontSize: '1.25rem',
+	box([
+		box(),
+		box([txt('Category 1'), txt('Category 2'), txt('Case studies')]).css({
 			display: 'flex',
-			justifyContent: 'space-between',
-			alignItems: 'center',
-			padding: '1.75rem',
-		})
+			gap: '2.5rem',
+		}),
+		btn('Sign up').css({
+			border: '1px solid',
+			borderRadius: '9999px',
+			padding: '1rem 3.5rem',
+			boxShadow: theme.shadows.normal,
+		}),
+	]).css({
+		fontSize: '1.25rem',
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: `1.75rem ${theme.container.padding}`,
+	})
 
 const hero = () =>
-	box().populate([
-		infoTxt(),
-		grid(2).populate([
-			stat({ title: '200 Users', sub: 'Every week' }),
-			stat({ title: '300 Subs', sub: 'Winning together' }),
-			stat({ title: '700K+', sub: 'Daily views' }),
-			stat({ title: '90+', sub: 'Sessions' }),
-		]),
-	])
+	grid(2)
+		.populate([
+			box([
+				infoTxt().css({
+					paddingTop: '6rem',
+				}),
+				grid(2)
+					.populate([
+						stat({ title: '200 Users', sub: 'Every week' }),
+						stat({ title: '300 Subs', sub: 'Winning together' }),
+						stat({ title: '700K+', sub: 'Daily views' }),
+						stat({ title: '90+', sub: 'Sessions' }),
+					])
+					.css({
+						marginTop: '4.125rem',
+						gap: '5rem',
+						width: '50%',
+					}),
+			]),
+			img(),
+		])
+		.css({
+			gridTemplateColumns: '3fr 2fr',
+			paddingLeft: theme.container.padding,
+		})
 
 const infoTxt = () =>
-	box().populate([
-		txt('BUILD YOUR COMMUNITY FASTER THAN EVER'),
+	box([
+		txt('BUILD YOUR COMMUNITY FASTER THAN EVER').css({
+			fontWeight: '700',
+			fontSize: '4rem',
+			lineHeight: '125%',
+		}),
 		txt(
 			'Elevate your online presence and expand your reach by connecting with like-minded individuals, engaging with your audience, and growing your community faster than ever before.'
-		),
-		btn('Join now!'),
+		).css({
+			color: theme.colors.textLight,
+			marginTop: '1.25rem',
+		}),
+		btn('Join now!').css({
+			backgroundColor: theme.colors.text,
+			boxShadow: theme.shadows.normal,
+			borderRadius: '9999px',
+			fontSize: '1.25rem',
+			color: theme.colors.background,
+			marginTop: '4.625rem',
+			padding: '1rem 2.8rem',
+		}),
 	])
 
 const stat = ({ title, sub }: { title: string; sub: string }) =>
-	box().populate([box(), txt(title), txt(sub)])
+	box([
+		box().css({
+			width: '1.3rem',
+			height: '0.3rem',
+			backgroundColor: theme.colors.primaryDark,
+		}),
+		txt(title).css({
+			fontWeight: '700',
+			fontSize: '1.5rem',
+			marginTop: '0.6rem',
+		}),
+		txt(sub).css({
+			color: theme.colors.textLight,
+			marginTop: '0.6rem',
+		}),
+	])
 
 const logos = () => grid(5).populate([img(), img(), img(), img(), img()])
 
@@ -104,10 +148,10 @@ const features = () =>
 	])
 
 const feature = ({ title, sub }: { title: string; sub: string }) =>
-	box().populate([img(), txt(title), txt(sub)])
+	box([img(), txt(title), txt(sub)])
 
 const info = () => grid(2).populate([infoTxt(), img()])
 
-const cta = () => box().populate([btn('Get the course now for $99 →')])
+const cta = () => box([btn('Get the course now for $99 →')])
 
 export { page as sofa }
