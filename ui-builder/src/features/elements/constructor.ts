327d06e2ed943303d3fc89712c68c02336e42199
frustak <soulburn007@gmail.com>
2023-01-30 13:03:29 +0330

    unstyle btn and txt

--- ui-builder/src/features/elements/constructor.ts
+++ ui-builder/src/features/elements/constructor.ts
@@ -5,7 +5,7 @@ import { ImageElement } from './extensions/image'
 import { TextElement } from './extensions/text'
 
 export const box = () => new BoxElement()
-export const btn = (txt: string) => new ButtonElement().txt(txt)
-export const txt = (txt: string) => new TextElement().txt(txt)
+export const btn = (txt: string) => new ButtonElement().unstyled().txt(txt)
+export const txt = (txt: string) => new TextElement().unstyled().txt(txt)
 export const grid = (columns: number) => new ColumnsElement().cols(columns)
 export const img = () => new ImageElement()
