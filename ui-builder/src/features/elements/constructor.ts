7e5ccb0cbb8f8f00af9ef6ba415b063d8f0764d3
frustak <soulburn007@gmail.com>
2023-01-30 13:31:30 +0330

    add children to box function

--- ui-builder/src/features/elements/constructor.ts
+++ ui-builder/src/features/elements/constructor.ts
@@ -1,10 +1,11 @@
+import { Element } from './element'
 import { BoxElement } from './extensions/box'
 import { ButtonElement } from './extensions/button'
 import { ColumnsElement } from './extensions/columns'
 import { ImageElement } from './extensions/image'
 import { TextElement } from './extensions/text'
 
-export const box = () => new BoxElement()
+export const box = (children?: Element[]) => new BoxElement().populate(children ?? [])
 export const btn = (txt: string) => new ButtonElement().unstyled().txt(txt)
 export const txt = (txt: string) => new TextElement().unstyled().txt(txt)
 export const grid = (columns: number) => new ColumnsElement().cols(columns)
