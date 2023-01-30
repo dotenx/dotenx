a99187f03aeb24abd7d0a18cbd7186d6aaec5b8e
frustak <soulburn007@gmail.com>
2023-01-30 12:48:00 +0330

    add inter font

--- ui-builder/src/features/style/generate-styles.ts
+++ ui-builder/src/features/style/generate-styles.ts
@@ -10,6 +10,7 @@ import { fontsAtom } from './typography-editor'
 
 const globalPageStyles = `
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
 *, *::before, *::after {
 	box-sizing: border-box;
   }
@@ -36,6 +37,10 @@ const globalPageStyles = `
   }
   #root, #__next {
 	isolation: isolate;
+  }
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `
 
