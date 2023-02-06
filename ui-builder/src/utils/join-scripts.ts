3eb5ce9010c85cbf59860847c633e4820eb37018
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-02-06 14:25:11 +0330

    improve join scripts function

--- ui-builder/src/utils/join-scripts.ts
+++ ui-builder/src/utils/join-scripts.ts
@@ -1,7 +1,11 @@
 import { Element } from '../features/elements/element'
 
 export function joinScripts(elements: Element[]): string {
+	if (elements.length === 0) {
+		return ''
+	}
+
 	return elements
-		.map((element) => joinScripts(element.children ?? []) + '\n' + element.script)
+		.map((element) => joinScripts(element.children ?? []) + '\n' + (element.script ?? ''))
 		.join('\n')
 }
