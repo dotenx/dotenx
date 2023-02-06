21c1248c4b5a428be434e2e1fef535a7b6061204
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-02-06 14:24:49 +0330

    add template constructor

--- ui-builder/src/features/elements/constructor.ts
+++ ui-builder/src/features/elements/constructor.ts
@@ -4,6 +4,7 @@ import { ButtonElement } from './extensions/button'
 import { ColumnsElement } from './extensions/columns'
 import { IconElement } from './extensions/icon'
 import { ImageElement } from './extensions/image'
+import { TemplateElement } from './extensions/template'
 import { TextElement } from './extensions/text'
 
 export const box = (children?: Element[]) => new BoxElement().populate(children ?? [])
@@ -12,3 +13,4 @@ export const txt = (txt: string) => new TextElement().unstyled().txt(txt)
 export const grid = (columns: number) => new ColumnsElement().cols(columns)
 export const img = (src?: string) => new ImageElement().src(src ?? '')
 export const icn = (name: string) => new IconElement().type('fas').setName(name)
+export const template = (children: Element) => new TemplateElement().populate([children])
