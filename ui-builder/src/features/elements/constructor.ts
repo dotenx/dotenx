c8dadd50643aa6fdac27917228d4ba0d19cbca77
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-02-08 10:19:56 +0330

    add link constructor

--- ui-builder/src/features/elements/constructor.ts
+++ ui-builder/src/features/elements/constructor.ts
@@ -4,12 +4,14 @@ import { ButtonElement } from './extensions/button'
 import { ColumnsElement } from './extensions/columns'
 import { IconElement } from './extensions/icon'
 import { ImageElement } from './extensions/image'
+import { LinkElement } from './extensions/link'
 import { TemplateElement } from './extensions/template'
 import { TextElement } from './extensions/text'
 
 export const box = (children?: Element[]) => new BoxElement().populate(children ?? [])
 export const btn = (txt: string) => new ButtonElement().unstyled().txt(txt)
 export const txt = (txt: string) => new TextElement().unstyled().txt(txt)
+export const link = () => new LinkElement().unstyled()
 export const grid = (columns: number) => new ColumnsElement().cols(columns)
 export const img = (src?: string) => new ImageElement().src(src ?? '')
 export const icn = (name: string) => new IconElement().type('fas').setName(name)
