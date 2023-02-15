c5a7c11ab89670f8c8372e94cac82f1718dd1fd3
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-02-15 12:19:00 +0330

    config editorjs

---
+++ ui-ecommerce/src/features/editor/editor.tsx
@@ -0,0 +1,29 @@
+import EditorJS from "@editorjs/editorjs"
+import { Button } from "@mantine/core"
+import edjsHTML from "editorjs-html"
+import { useEffect, useRef } from "react"
+import { createEditor, EDITOR_ID } from "./config"
+
+const edjsParser = edjsHTML()
+
+export function Editor() {
+	const editorRef = useRef<EditorJS | null>(null)
+
+	useEffect(() => {
+		if (editorRef.current) return
+		editorRef.current = createEditor()
+	}, [])
+
+	const handleSave = async () => {
+		const data = await editorRef.current?.save()
+		const html = edjsParser.parse(data)
+		console.log(html)
+	}
+
+	return (
+		<div className="bg-white rounded-md p-6 prose max-w-none">
+			<div id={EDITOR_ID} />
+			<Button onClick={handleSave}>Save</Button>
+		</div>
+	)
+}
