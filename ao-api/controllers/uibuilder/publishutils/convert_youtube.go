7067624cf1afef37840be7dda2686f3dca8c0d2f
Mohsen Kamrani <m.kamrani@hotmail.com>
2023-02-24 12:56:39 +1100

    Render video and YouTube

---
+++ ao-api/controllers/uibuilder/publishutils/convert_youtube.go
@@ -0,0 +1,80 @@
+package publishutils
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"text/template"
+)
+
+type YouTube struct {
+	Kind       string        `json:"kind"`
+	Id         string        `json:"id"`
+	Components []interface{} `json:"components"`
+	Events     []Event       `json:"events"`
+	Bindings   Bindings      `json:"bindings"`
+	RepeatFrom struct {
+		Name     string
+		Iterator string
+	} `json:"repeatFrom"`
+	ClassNames []string `json:"classNames"`
+	ElementId  string   `json:"elementId"`
+	Data       struct {
+		Style struct {
+			Desktop StyleModes `json:"desktop"`
+			Tablet  StyleModes `json:"tablet"`
+			Mobile  StyleModes `json:"mobile"`
+		} `json:"style"`
+		Src string `json:"src"`
+	} `json:"data"`
+}
+
+const youtubeTemplate = `{{if .RepeatFrom.Name}}<template x-for="({{.RepeatFrom.Iterator}}, index) in {{renderRepeatFromName .RepeatFrom.Name}}">{{end}}<iframe {{if .Bindings.Class.FromStateName}}:class="{{renderClassBinding .Bindings}}"{{end}} {{if or .Bindings.Show.FromStateName .Bindings.Hide.FromStateName}}x-show="{{renderBindings .Bindings}}"{{end}}  {{renderEvents .Events}} {{if .RepeatFrom.Name}}:key="index"{{end}} id="{{if .ElementId}}{{.ElementId}}{{else}}{{.Id}}{{end}}" class="{{range .ClassNames}}{{.}} {{end}}" src="{{.Data.Src}}" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />{{if .RepeatFrom.Name}}</template>{{end}}`
+
+func convertYouTube(component map[string]interface{}, styleStore *StyleStore, functionStore *FunctionStore) (string, error) {
+
+	funcMap := template.FuncMap{
+		"renderClassBinding":   RenderClassBinding,
+		"renderBindings":       RenderShowHideBindings,
+		"renderEvents":         renderEvents,
+		"renderRepeatFromName": renderRepeatFromName,
+	}
+
+	b, err := json.Marshal(component)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	var youtube YouTube
+	json.Unmarshal(b, &youtube)
+	tmpl, err := template.New("youtube").Funcs(funcMap).Parse(youtubeTemplate)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	params := struct {
+		YouTube
+	}{
+		youtube,
+	}
+
+	var out bytes.Buffer
+	err = tmpl.Execute(&out, params)
+	if err != nil {
+		fmt.Println("error: ", err.Error())
+		return "", err
+	}
+
+	// Add the events to the function store to be rendered later
+	functionStore.AddEvents(youtube.Events)
+
+	// Add the styles to the styleStore to be rendered later
+	id := youtube.Id
+	if youtube.ElementId != "" {
+		id = youtube.ElementId
+	}
+	styleStore.AddStyle(id, youtube.Data.Style.Desktop, youtube.Data.Style.Tablet, youtube.Data.Style.Mobile)
+
+	return out.String(), nil
+}
