a5a4fb208864c72eb350662e50bcf6b93ff82e53
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-03-14 13:48:48 +0330

    add rich text editor to text styler

--- ui-builder/src/features/simple/stylers/text-styler.tsx
+++ ui-builder/src/features/simple/stylers/text-styler.tsx
@@ -1,10 +1,13 @@
-import { TextInput } from '@mantine/core'
+import { ActionIcon, TextInput } from '@mantine/core'
+import { closeAllModals, openModal } from '@mantine/modals'
 import _ from 'lodash'
+import { TbEdit } from 'react-icons/tb'
 import { useSetWithElement } from '../../elements/elements-store'
 import { TextElement } from '../../elements/extensions/text'
 import { Expression } from '../../states/expression'
 import { SpacingEditor } from '../../style/spacing-editor'
 import { TypographyEditor } from '../../style/typography-editor'
+import { TextEditor } from '../../ui/text-editor'
 import { Styler } from './styler'
 
 export function TextStyler({
@@ -13,12 +16,14 @@ export function TextStyler({
 	placeholder,
 	noText,
 	onChange,
+	rich,
 }: {
 	label: string
 	element: TextElement | TextElement[]
 	placeholder?: string
 	noText?: boolean
 	onChange?: (text: string) => void
+	rich?: boolean
 }) {
 	if (noText) return <TextStylerNoText label={label} element={element} />
 
@@ -28,6 +33,7 @@ export function TextStyler({
 			element={_.isArray(element) ? element[0] : element}
 			placeholder={placeholder}
 			onChange={onChange}
+			rich={rich}
 		/>
 	)
 }
@@ -37,15 +43,36 @@ function TextStylerWithText({
 	element,
 	placeholder,
 	onChange,
+	rich,
 }: {
 	label: string
 	element: TextElement
 	placeholder?: string
 	onChange?: (text: string) => void
+	rich?: boolean
 }) {
 	const set = useSetWithElement(element)
 	const setText = (text: string) => {
 		set((draft) => (draft.data.text = Expression.fromString(text)))
+	}
+
+	const value = element.data.text.toString()
+	const isHtml = value.startsWith('<') && value.endsWith('>')
+
+	const openEditor = () => {
+		closeAllModals()
+		openModal({
+			children: (
+				<TextEditor
+					onSave={(html) => {
+						setText(html)
+						onChange?.(html)
+					}}
+					content={value}
+				/>
+			),
+			size: 'xl',
+		})
 	}
 
 	return (
@@ -54,12 +81,25 @@ function TextStylerWithText({
 			label={label}
 			name={label}
 			placeholder={placeholder}
-			value={element.data.text.toString()}
+			value={isHtml ? 'Open editor to edit' : value}
+			onClick={isHtml ? openEditor : undefined}
+			readOnly={isHtml}
 			onChange={(event) => {
 				setText(event.target.value)
 				onChange?.(event.target.value)
 			}}
-			rightSection={<StyleEditor element={element} />}
+			rightSection={
+				rich ? (
+					<div className="flex gap-1 bg-white">
+						<ActionIcon size="xs" onClick={openEditor}>
+							<TbEdit size={12} />
+						</ActionIcon>
+						<StyleEditor element={element} />
+					</div>
+				) : (
+					<StyleEditor element={element} />
+				)
+			}
 		/>
 	)
 }
---
+++ ui-builder/src/features/ui/text-editor.tsx
@@ -0,0 +1,98 @@
+import { Link, RichTextEditor } from '@mantine/tiptap'
+import { Color } from '@tiptap/extension-color'
+import TextStyle from '@tiptap/extension-text-style'
+
+import { Button } from '@mantine/core'
+import { closeAllModals } from '@mantine/modals'
+import Highlight from '@tiptap/extension-highlight'
+import SubScript from '@tiptap/extension-subscript'
+import Superscript from '@tiptap/extension-superscript'
+import TextAlign from '@tiptap/extension-text-align'
+import Underline from '@tiptap/extension-underline'
+import { useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { useAtomValue } from 'jotai'
+import { selectedPaletteAtom } from '../simple/palette'
+
+export function TextEditor({
+	onSave,
+	content,
+}: {
+	onSave: (html: string) => void
+	content: string
+}) {
+	const editor = useEditor({
+		extensions: [
+			StarterKit,
+			Underline,
+			Link,
+			Superscript,
+			SubScript,
+			Highlight,
+			TextStyle,
+			Color,
+			TextAlign.configure({ types: ['heading', 'paragraph'] }),
+		],
+		content,
+	})
+	const selectedPalette = useAtomValue(selectedPaletteAtom)
+
+	return (
+		<div>
+			<RichTextEditor editor={editor}>
+				<RichTextEditor.Toolbar sticky stickyOffset={60}>
+					<RichTextEditor.ControlsGroup>
+						<RichTextEditor.Bold />
+						<RichTextEditor.Italic />
+						<RichTextEditor.Underline />
+						<RichTextEditor.Strikethrough />
+						<RichTextEditor.ClearFormatting />
+						<RichTextEditor.Highlight />
+						<RichTextEditor.ColorPicker colors={selectedPalette.colors} />
+						<RichTextEditor.Code />
+					</RichTextEditor.ControlsGroup>
+
+					<RichTextEditor.ControlsGroup>
+						<RichTextEditor.H1 />
+						<RichTextEditor.H2 />
+						<RichTextEditor.H3 />
+						<RichTextEditor.H4 />
+					</RichTextEditor.ControlsGroup>
+
+					<RichTextEditor.ControlsGroup>
+						<RichTextEditor.Blockquote />
+						<RichTextEditor.Hr />
+						<RichTextEditor.BulletList />
+						<RichTextEditor.OrderedList />
+						<RichTextEditor.Subscript />
+						<RichTextEditor.Superscript />
+					</RichTextEditor.ControlsGroup>
+
+					<RichTextEditor.ControlsGroup>
+						<RichTextEditor.Link />
+						<RichTextEditor.Unlink />
+					</RichTextEditor.ControlsGroup>
+
+					<RichTextEditor.ControlsGroup>
+						<RichTextEditor.AlignLeft />
+						<RichTextEditor.AlignCenter />
+						<RichTextEditor.AlignJustify />
+						<RichTextEditor.AlignRight />
+					</RichTextEditor.ControlsGroup>
+				</RichTextEditor.Toolbar>
+
+				<RichTextEditor.Content />
+			</RichTextEditor>
+
+			<Button
+				mt="xl"
+				onClick={() => {
+					onSave(editor?.getHTML() ?? '')
+					closeAllModals()
+				}}
+			>
+				Save
+			</Button>
+		</div>
+	)
+}
