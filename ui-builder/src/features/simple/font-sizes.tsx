114b8fc4f1a99a9165ec229cd58103a862bbd969
Utopiops <utopiops@gmail.com>
2023-03-14 23:44:54 +0000

    Dtx 732 - Add team-square-grid

---
+++ ui-builder/src/features/simple/font-sizes.tsx
@@ -0,0 +1,27 @@
+export const fontSizes = {
+	h1: {
+		desktop: '36px',
+		tablet: '32px',
+		mobile: '28px',
+	},
+	h2: {
+		desktop: '28px',
+		tablet: '24px',
+		mobile: '20px',
+	},
+	h3: {
+		desktop: '22px',
+		tablet: '20px',
+		mobile: '18px',
+	},
+	h4: {
+		desktop: '18px',
+		tablet: '16px',
+		mobile: '14px',
+	},
+	normal: {
+		desktop: '16px',
+		tablet: '14px',
+		mobile: '12px',
+	},
+}
