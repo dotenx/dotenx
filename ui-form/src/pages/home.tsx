34aa13a78a2d61e60de5f7dc096a8eda41db18bc
mehdi65 <mehdi@utopiops.com>
2023-03-15 10:37:13 +0330

    change form design

--- ui-form/src/pages/home.tsx
+++ ui-form/src/pages/home.tsx
@@ -1,6 +1,5 @@
-import { Anchor } from "@mantine/core"
 import { useQuery } from "@tanstack/react-query"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { api } from "../api"
 import { QueryKey } from "../api/types"
 import { useGetProjectTag } from "../features/hooks/use-project-query"
@@ -20,28 +19,38 @@ export function HomePage() {
 
 function PageList() {
 	const pagesQuery = usePagesQuery()
+	const navigate = useNavigate()
 
 	if (pagesQuery.isLoading) return <Loader />
 
 	return (
-		<ul className="space-y-2">
-			{pagesQuery.data?.data?.map((page) => (
-				<li key={page}>
-					<Anchor component={Link} to={page} color="dimmed">
-						{page}
-					</Anchor>
-				</li>
-			))}
-		</ul>
+		<div>
+			<div className="flex text-gray-600 justify-between w-full mb-3 mt-5 px-5">
+				<div>Name</div>
+				<div>Submitted forms</div>
+			</div>
+			<div className="space-y-3">
+				{pagesQuery?.data?.data?.map((page) => (
+					<div
+						className="cursor-pointer hover:shadow-lg flex transition-all hover:bg-black hover:text-white justify-between w-full bg-white text-xl font-semibold p-5  rounded-md"
+						key={page.page_name}
+						onClick={() => navigate(page.page_name)}
+					>
+						<div>{page.page_name}</div>
+						<div className="min-w-[100px] text-center">{page.submitted_forms}</div>
+					</div>
+				))}
+			</div>
+		</div>
 	)
 }
 
 const usePagesQuery = () => {
 	const { projectTag } = useGetProjectTag()
 	return useQuery(
-		[QueryKey.GetPages, projectTag],
+		[QueryKey.GetPagesList, projectTag],
 		() =>
-			api.getPages({
+			api.getPagesList({
 				projectTag,
 			}),
 		{
