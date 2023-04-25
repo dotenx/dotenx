ec1c159136653fcd13892b5b2e07f608126c76b5
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-04-25 12:42:52 +0430

    update image

--- ui-builder/src/features/components/features/feature-7.tsx
+++ ui-builder/src/features/components/features/feature-7.tsx
@@ -1,5 +1,5 @@
 // relume feature 31
-import componentImage from '../../../assets/components/features/feature-1.png'
+import componentImage from '../../../assets/components/features/feature-7.png'
 import { gridCols } from '../../../utils/style-utils'
 import { box, container, flex, grid, icn, img, link, txt } from '../../elements/constructor'
 import { BoxElement } from '../../elements/extensions/box'
