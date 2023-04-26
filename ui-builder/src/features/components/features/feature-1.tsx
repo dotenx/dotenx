46cecdc14adab56cee434f60e59b7cc5da1066ef
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-04-26 20:31:32 +0430

    extract icon subheadings

--- ui-builder/src/features/components/features/feature-1.tsx
+++ ui-builder/src/features/components/features/feature-1.tsx
@@ -3,19 +3,16 @@ import componentImage from '../../../assets/components/features/feature-1.png'
 import { gridCols } from '../../../utils/style-utils'
 import { box, container, flex, grid, icn, img, link, txt } from '../../elements/constructor'
 import { BoxElement } from '../../elements/extensions/box'
-import { IconElement } from '../../elements/extensions/icon'
 import { ImageElement } from '../../elements/extensions/image'
 import { LinkElement } from '../../elements/extensions/link'
 import { TextElement } from '../../elements/extensions/text'
 import { useSelectedElement } from '../../selection/use-selected-component'
-import { IconStyler } from '../../simple/stylers/icon-styler'
 import { ImageStyler } from '../../simple/stylers/image-styler'
 import { LinkStyler } from '../../simple/stylers/link-styler'
 import { TextStyler } from '../../simple/stylers/text-styler'
 import { Component } from '../component'
 import { ComponentWrapper } from '../helpers/component-wrapper'
-import { DndTabs } from '../helpers/dnd-tabs'
-import { OptionsWrapper } from '../helpers/options-wrapper'
+import { cmn } from './common'
 
 export class Feature1 extends Component {
 	name = 'Feature 1'
@@ -30,7 +27,6 @@ function Options() {
 	const tagline = component.find<TextElement>(tags.tagline)!
 	const heading = component.find<TextElement>(tags.heading)!
 	const description = component.find<TextElement>(tags.description)!
-	const subheadingList = component.find<BoxElement>(tags.subheading.list)!
 	const link1 = component.find<LinkElement>(tags.link1)!
 	const link1Text = link1.find<TextElement>(tags.link1Text)!
 	const link2 = component.find<LinkElement>(tags.link2)!
@@ -46,26 +42,8 @@ function Options() {
 			<TextStyler label="Link 1 text" element={link1Text} />
 			<LinkStyler label="Link 2" element={link2} />
 			<TextStyler label="Link 2 text" element={link2Text} />
-			<DndTabs
-				containerElement={subheadingList}
-				insertElement={() => subheading('Subheading')}
-				renderItemOptions={(item) => <ItemOptions item={item as BoxElement} />}
-			/>
+			<cmn.icnSubheading.Options />
 		</ComponentWrapper>
-	)
-}
-
-function ItemOptions({ item }: { item: BoxElement }) {
-	const title = item.find<TextElement>(tags.subheading.title)!
-	const description = item.find<TextElement>(tags.subheading.description)!
-	const icon = item.find<IconElement>(tags.subheading.icon)!
-
-	return (
-		<OptionsWrapper>
-			<TextStyler label="Title" element={title} />
-			<TextStyler label="Description" element={description} />
-			<IconStyler label="Icon" element={icon} />
-		</OptionsWrapper>
 	)
 }
 
@@ -73,12 +51,6 @@ const tags = {
 	tagline: 'tagline',
 	heading: 'heading',
 	description: 'description',
-	subheading: {
-		list: 'list',
-		title: 'title',
-		description: 'description',
-		icon: 'icon',
-	},
 	link1: 'link1',
 	link1Text: 'link1Text',
 	link2: 'link2',
@@ -123,17 +95,7 @@ const component = () =>
 								fontSize: '1rem',
 							})
 							.tag(tags.description),
-						grid(2)
-							.populate([subheading('Subheading one'), subheading('Subheading one')])
-							.css({
-								paddingTop: '0.5rem',
-								paddingBottom: '0.5rem',
-								gap: '1.5rem',
-							})
-							.cssTablet({
-								gridTemplateColumns: gridCols(1),
-							})
-							.tag(tags.subheading.list),
+						cmn.icnSubheading.list(),
 						flex([
 							link()
 								.populate([txt('Button').tag(tags.link1Text)])
@@ -180,28 +142,3 @@ const component = () =>
 			paddingTop: '4rem',
 			paddingBottom: '4rem',
 		})
-
-const subheading = (title: string) =>
-	flex([
-		icn('square').size('32px').tag(tags.subheading.icon),
-		box([
-			txt(title)
-				.css({
-					fontWeight: '700',
-					fontSize: '1.25rem',
-					lineHeight: '1.4',
-					marginBottom: '1rem',
-				})
-				.tag(tags.subheading.title),
-			txt(
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.'
-			)
-				.css({
-					fontSize: '1rem',
-					lineHeight: '1.5',
-				})
-				.tag(tags.subheading.description),
-		]),
-	]).css({
-		gap: '1rem',
-	})
