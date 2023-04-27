79017cc3276ae9e3f19794ae8f2dc50f87fec718
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-04-27 16:15:35 +0430

    feature 24

--- ui-builder/src/features/components/features/feature-24.tsx
+++ ui-builder/src/features/components/features/feature-24.tsx
@@ -1,12 +1,12 @@
-// relume feature 138
-import componentImage from '../../../assets/components/features/feature-23.png'
+// relume feature 141
+import componentImage from '../../../assets/components/features/feature-24.png'
 import { column } from '../../elements/constructor'
 import { Component } from '../component'
 import { ComponentWrapper } from '../helpers/component-wrapper'
 import { cmn } from './common'
 
-export class Feature23 extends Component {
-	name = 'Feature 23'
+export class Feature24 extends Component {
+	name = 'Feature 24'
 	image = componentImage
 	defaultData = component()
 	renderOptions = () => <Options />
@@ -19,23 +19,18 @@ function Options() {
 			<cmn.heading.Options />
 			<cmn.desc.Options />
 			<cmn.btnLinks.Options />
-			<cmn.brands.Options />
+			<cmn.fullImg.Options />
 		</ComponentWrapper>
 	)
 }
 
 const component = () =>
 	cmn.ppr.el([
-		column([
-			cmn.tagline.el(),
-			cmn.heading.el(),
-			cmn.desc.el(),
-			cmn.brands.el(),
-			cmn.btnLinks.el(),
-		]).css({
+		column([cmn.tagline.el(), cmn.heading.el(), cmn.desc.el(), cmn.btnLinks.el()]).css({
 			alignItems: 'center',
 			textAlign: 'center',
 			maxWidth: '48rem',
 			margin: '0 auto',
 		}),
+		cmn.fullImg.el(),
 	])
