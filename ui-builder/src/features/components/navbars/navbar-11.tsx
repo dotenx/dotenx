d9aa371d8c7c33bea66085bd8a344f12be92dcdd
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-05-10 12:27:26 +0430

    navbar 11 options

--- ui-builder/src/features/components/navbars/navbar-11.tsx
+++ ui-builder/src/features/components/navbars/navbar-11.tsx
@@ -1,8 +1,19 @@
+import { ActionIcon, Menu } from '@mantine/core'
+import { TbPlus } from 'react-icons/tb'
 import componentImage from '../../../assets/components/navbar/navbar-11.png'
 import { box, flex } from '../../elements/constructor'
+import { useSetElement } from '../../elements/elements-store'
+import { BoxElement } from '../../elements/extensions/box'
+import { useSelectedElement } from '../../selection/use-selected-component'
 import { Component } from '../component'
 import { ComponentWrapper } from '../helpers/component-wrapper'
+import { DndTabs } from '../helpers/dnd-tabs'
+import { OptionsWrapper } from '../helpers/options-wrapper'
 import { cmn } from './common/navbar'
+
+const tags = {
+	list: 'list',
+}
 
 export class Navbar11 extends Component {
 	name = 'Navbar 11'
@@ -16,8 +27,75 @@ function Options() {
 		<ComponentWrapper>
 			<cmn.logo.Options />
 			<cmn.buttons.Options />
-			<cmn.linkList.Options />
+			<LinkListOptions />
 		</ComponentWrapper>
+	)
+}
+
+function LinkListOptions() {
+	const set = useSetElement()
+	const component = useSelectedElement() as BoxElement
+	const container = component.find(cmn.tags.linkList.container) as BoxElement
+	return (
+		<OptionsWrapper>
+			<DndTabs
+				containerElement={container}
+				rightSection={
+					<Menu position="left">
+						<Menu.Target>
+							<ActionIcon variant="transparent">
+								<TbPlus
+									size={16}
+									className="text-red-500 rounded-full border-red-500 border"
+								/>
+							</ActionIcon>
+						</Menu.Target>
+						<Menu.Dropdown>
+							<Menu.Item
+								onClick={() =>
+									set(container, (draft) =>
+										draft.children.push(cmn.linkItem.el('Link'))
+									)
+								}
+							>
+								Text Link
+							</Menu.Item>
+							<Menu.Item
+								onClick={() =>
+									set(container, (draft) => draft.children.push(createMenuItem()))
+								}
+							>
+								Menu Link
+							</Menu.Item>
+						</Menu.Dropdown>
+					</Menu>
+				}
+				renderItemOptions={(item) => <LinkListItemOptions item={item as BoxElement} />}
+			/>
+		</OptionsWrapper>
+	)
+}
+
+function LinkListItemOptions({ item }: { item: BoxElement }) {
+	return (
+		<OptionsWrapper>
+			<cmn.linkItem.Options root={item} />
+			<LinkMenuOptions item={item} />
+		</OptionsWrapper>
+	)
+}
+
+function LinkMenuOptions({ item }: { item: BoxElement }) {
+	const list = item.find(tags.list) as BoxElement
+	if (!list) return null
+	return (
+		<OptionsWrapper>
+			<DndTabs
+				containerElement={list}
+				renderItemOptions={(item) => <cmn.menuItem.Options item={item as BoxElement} />}
+				insertElement={() => cmn.menuItem.el('Page')}
+			/>
+		</OptionsWrapper>
 	)
 }
 
@@ -45,18 +123,22 @@ const linkList = () =>
 		cmn.linkItem.el('Link One'),
 		cmn.linkItem.el('Link Two'),
 		cmn.linkItem.el('Link Three'),
-		cmn.linkMenu.el('Link Four', [
-			cmn.linkSubmenu
-				.el(['Page One', 'Page Two', 'Page Three', 'Page Four'].map(cmn.menuItem.el))
-				.css({
-					width: '20rem',
-				})
-				.cssTablet({
-					width: 'auto',
-				}),
-		]),
+		createMenuItem(),
 	])
 		.cssTablet({
 			flexDirection: 'column',
 		})
 		.tag(cmn.tags.linkList.container)
+
+const createMenuItem = () =>
+	cmn.linkMenu.el('Link Four', [
+		cmn.linkSubmenu
+			.el(['Page One', 'Page Two', 'Page Three', 'Page Four'].map(cmn.menuItem.el))
+			.css({
+				width: '20rem',
+			})
+			.cssTablet({
+				width: 'auto',
+			})
+			.tag(tags.list),
+	])
