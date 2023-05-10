4530e20e6e9357788ed65d8ef1e793548175a85a
erfanqasemizade <erfan.qasemizade@utopiops.com>
2023-05-10 10:38:56 +0430

    navbar 7

--- ui-builder/src/features/components/navbars/navbar-7.tsx
+++ ui-builder/src/features/components/navbars/navbar-7.tsx
@@ -1,9 +1,25 @@
+import { ActionIcon, Menu } from '@mantine/core'
+import { TbPlus } from 'react-icons/tb'
 import componentImage from '../../../assets/components/navbar/navbar-7.png'
 import { gridCols } from '../../../utils/style-utils'
-import { box, flex, grid, icn, txt } from '../../elements/constructor'
+import { box, flex, grid } from '../../elements/constructor'
+import { useSetElement } from '../../elements/elements-store'
+import { BoxElement } from '../../elements/extensions/box'
+import { useSelectedElement } from '../../selection/use-selected-component'
 import { Component } from '../component'
 import { ComponentWrapper } from '../helpers/component-wrapper'
+import { DndTabs } from '../helpers/dnd-tabs'
+import { OptionsWrapper } from '../helpers/options-wrapper'
 import { cmn } from './common/navbar'
+
+const tags = {
+	pageGroups: 'pageGroups',
+	featured: {
+		title: 'featured.title',
+		arrowLink: 'featured.arrowLink',
+		arrowLinkText: 'featured.arrowLinkText',
+	},
+}
 
 export class Navbar7 extends Component {
 	name = 'Navbar 7'
@@ -13,7 +29,83 @@ export class Navbar7 extends Component {
 }
 
 function Options() {
-	return <ComponentWrapper></ComponentWrapper>
+	return (
+		<ComponentWrapper>
+			<cmn.logo.Options />
+			<cmn.buttons.Options />
+			<LinkListOptions />
+		</ComponentWrapper>
+	)
+}
+
+function LinkListOptions() {
+	const set = useSetElement()
+	const component = useSelectedElement() as BoxElement
+	const container = component.find(cmn.tags.linkList.container) as BoxElement
+	return (
+		<OptionsWrapper>
+			<DndTabs
+				containerElement={container}
+				rightSection={
+					<Menu position="left">
+						<Menu.Target>
+							<ActionIcon variant="transparent">
+								<TbPlus
+									size={16}
+									className="text-red-500 rounded-full border-red-500 border"
+								/>
+							</ActionIcon>
+						</Menu.Target>
+						<Menu.Dropdown>
+							<Menu.Item
+								onClick={() =>
+									set(container, (draft) =>
+										draft.children.push(cmn.linkItem.el('Link'))
+									)
+								}
+							>
+								Text Link
+							</Menu.Item>
+							<Menu.Item
+								onClick={() =>
+									set(container, (draft) => draft.children.push(createMenuItem()))
+								}
+							>
+								Menu Link
+							</Menu.Item>
+						</Menu.Dropdown>
+					</Menu>
+				}
+				renderItemOptions={(item) => <LinkListItemOptions item={item as BoxElement} />}
+			/>
+		</OptionsWrapper>
+	)
+}
+
+function LinkListItemOptions({ item }: { item: BoxElement }) {
+	return (
+		<OptionsWrapper>
+			<cmn.linkItem.Options root={item} />
+			<LinkMenuOptions item={item} />
+		</OptionsWrapper>
+	)
+}
+
+function LinkMenuOptions({ item }: { item: BoxElement }) {
+	const pageGroups = item.find(tags.pageGroups) as BoxElement
+	if (!pageGroups) return null
+	return (
+		<OptionsWrapper>
+			<DndTabs
+				containerElement={pageGroups}
+				renderItemOptions={(item) => <cmn.pageGroup.Options item={item as BoxElement} />}
+				insertElement={() =>
+					cmn.pageGroup.el('Page group ', ['Page', 'Page', 'Page', 'Page'])
+				}
+				maxLength={2}
+			/>
+		</OptionsWrapper>
+	)
 }
 
 const component = () =>
@@ -49,95 +141,72 @@ const menu = () =>
 			}),
 	])
 
+const createMenuItem = () =>
+	cmn.linkMenu
+		.el(
+			'Link Four',
+			[
+				cmn.linkSubmenu
+					.el([
+						grid(4)
+							.populate([
+								cmn.pageGroup.el('Page group one', [
+									'Page One',
+									'Page Two',
+									'Page Three',
+									'Page Four',
+								]),
+								cmn.pageGroup.el('Page group two', [
+									'Page Five',
+									'Page Six',
+									'Page Seven',
+									'Page Eight',
+								]),
+								cmn.pageGroup.el('Page group three', [
+									'Page Nine',
+									'Page Ten',
+									'Page Eleven',
+									'Page Twelve',
+								]),
+								cmn.pageGroup.el('Page group four', [
+									'Page Thirteen',
+									'Page Fourteen',
+									'Page Fifteen',
+									'Page Sixteen',
+								]),
+							])
+							.tag(tags.pageGroups)
+							.css({
+								padding: '2rem 5%',
+							})
+							.cssTablet({
+								gridTemplateColumns: gridCols(1),
+							}),
+					])
+					.css({
+						top: '100%',
+						right: '0',
+						left: '0',
+						width: 'auto',
+						border: 'none',
+						borderBottom: '1px solid #000',
+						padding: '0',
+					}),
+			],
+			false
+		)
+		.css({
+			position: 'unset',
+		})
+
 const linkList = () =>
 	flex([
 		cmn.linkItem.el('Link One'),
 		cmn.linkItem.el('Link Two'),
 		cmn.linkItem.el('Link Three'),
-		cmn.linkMenu
-			.el(
-				'Link Four',
-				[
-					cmn.linkSubmenu
-						.el([
-							grid(4)
-								.populate([
-									cmn.pageGroup.el('Page group one', [
-										'Page One',
-										'Page Two',
-										'Page Three',
-										'Page Four',
-									]),
-									cmn.pageGroup.el('Page group two', [
-										'Page Five',
-										'Page Six',
-										'Page Seven',
-										'Page Eight',
-									]),
-									cmn.pageGroup.el('Page group three', [
-										'Page Nine',
-										'Page Ten',
-										'Page Eleven',
-										'Page Twelve',
-									]),
-									cmn.pageGroup.el('Page group four', [
-										'Page Thirteen',
-										'Page Fourteen',
-										'Page Fifteen',
-										'Page Sixteen',
-									]),
-								])
-								.css({
-									padding: '2rem 5%',
-								})
-								.cssTablet({
-									gridTemplateColumns: gridCols(1),
-								}),
-							flex([
-								flex([
-									txt('Ready to get started?'),
-									txt(' Sign up for free').css({
-										textDecoration: 'underline',
-									}),
-								]).css({
-									gap: '1ch',
-								}),
-								flex([btn(), btn()]).css({
-									gap: '2rem',
-								}),
-							])
-								.css({
-									backgroundColor: '#F4F4F4',
-									padding: '1.5rem 5%',
-									justifyContent: 'space-between',
-									alignItems: 'center',
-								})
-								.cssTablet({
-									flexDirection: 'column',
-									gap: '1rem',
-								}),
-						])
-						.css({
-							top: '100%',
-							right: '0',
-							left: '0',
-							width: 'auto',
-							border: 'none',
-							borderBottom: '1px solid #000',
-							padding: '0',
-						}),
-				],
-				false
-			)
-			.css({
-				position: 'unset',
-			}),
-	]).cssTablet({
-		flexDirection: 'column',
-	})
-
-const btn = () =>
-	flex([icn('cube').size('14px'), txt('Button')]).css({
-		alignItems: 'center',
-		gap: '1rem',
-	})
+		createMenuItem(),
+	])
+		.cssTablet({
+			flexDirection: 'column',
+		})
+		.tag(cmn.tags.linkList.container)
