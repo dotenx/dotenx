8d88b697a0b114539e82e6c5792186a252a7f767
frustak <soulburn007@gmail.com>
2023-05-18 10:20:24 +0330

    add dynamic table

--- ui-builder/src/features/components/dynamics/table.tsx
+++ ui-builder/src/features/components/dynamics/table.tsx
@@ -1,11 +1,16 @@
+import { useAtomValue } from 'jotai'
 import _ from 'lodash'
 import componentImage from '../../../assets/components/faq/faq-1.png'
 import { box, grid, txt } from '../../elements/constructor'
 import { Element } from '../../elements/element'
-import { setElement } from '../../elements/elements-store'
+import { setElement, useSetElement } from '../../elements/elements-store'
+import { BoxElement } from '../../elements/extensions/box'
+import { projectTagAtom } from '../../page/top-bar'
 import componentScript from '../../scripts/dynamic/table.js?raw'
+import { useSelectedElement } from '../../selection/use-selected-component'
 import { cmn } from '../common'
-import { Component } from '../component'
+import { Component, OnCreateOptions } from '../component'
+import { TableSelect } from '../create-form'
 import { ComponentWrapper } from '../helpers/component-wrapper'
 
 export class DynamicTable extends Component {
@@ -13,46 +18,68 @@ export class DynamicTable extends Component {
 	image = componentImage
 	defaultData = component()
 	renderOptions = () => <Options />
-	onCreate(root: Element) {
+	onCreate(root: Element, options: OnCreateOptions) {
 		const compiled = _.template(componentScript)
-		const script = compiled({ id: root.id })
+		const script = compiled({
+			id: root.id,
+			projectTag: options.projectTag,
+			tableName: root.internal.tableName ?? '',
+		})
 		setElement(root, (draft) => (draft.script = script))
 	}
 }
 
 function Options() {
-	return <ComponentWrapper></ComponentWrapper>
+	const root = useSelectedElement() as BoxElement
+	const setElement = useSetElement()
+	const projectTag = useAtomValue(projectTagAtom)
+
+	return (
+		<ComponentWrapper>
+			<TableSelect
+				value={(root.internal.tableName as string) ?? ''}
+				onChange={(value) => {
+					const compiled = _.template(componentScript)
+					const script = compiled({
+						id: root.id,
+						projectTag: projectTag,
+						tableName: value,
+						columns: JSON.stringify(root.internal.columns ?? []),
+					})
+					setElement(root, (draft) => {
+						draft.internal.tableName = value
+						draft.script = script
+					})
+				}}
+			/>
+		</ComponentWrapper>
+	)
+}
+
+const tags = {
+	head: 'head',
+	row: 'row',
 }
 
 const component = () => cmn.ppr.el([box([head(), row(), row(), row()]).class('table')])
 
 const head = () =>
 	grid(3)
-		.populate([
-			txt('Name').css({
-				fontWeight: 'bold',
-			}),
-			txt('Username').css({
-				fontWeight: 'bold',
-			}),
-			txt('Email').css({
-				fontWeight: 'bold',
-			}),
-		])
+		.populate([txt('Column'), txt('Column'), txt('Column')])
 		.css({
 			gap: '1rem',
 			marginBottom: '1rem',
+			fontWeight: 'bold',
 		})
+		.tag(tags.head)
+		.class('head')
 
 const row = () =>
 	grid(3)
-		.populate([
-			txt('John Doe').class('name'),
-			txt('john_doe').class('username'),
-			txt('john.doe@email.com').class('email'),
-		])
+		.populate([txt('Cell'), txt('Cell'), txt('Cell')])
 		.css({
 			gap: '1rem',
 			marginBottom: '1rem',
 		})
+		.tag(tags.row)
 		.class('row')
--- ui-builder/src/features/scripts/dynamic/table.js
+++ ui-builder/src/features/scripts/dynamic/table.js
@@ -2,26 +2,44 @@
 
 ;(async () => {
 	const id = '{{id}}'
+	const projectTag = '{{projectTag}}'
+	const tableName = '{{tableName}}'
 
 	const root = document.getElementById(id)
 	const table = root.querySelector('.table')
-	const rows = table.querySelectorAll('.row')
-	const row = table.querySelector('.row')
+	const rows = root.querySelectorAll('.row')
+	const rowTemplate = root.querySelector('.row')
+	const head = root.querySelector('.head')
 
+	head.innerHTML = ''
 	rows.forEach((row) => row.remove())
 
-	const data = await fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json())
-	data.forEach((user) => {
-		const clone = row.cloneNode(true)
-		const name = clone.querySelector('.name')
-		const username = clone.querySelector('.username')
-		const email = clone.querySelector('.email')
-		name.removeAttribute('x-html')
-		username.removeAttribute('x-html')
-		email.removeAttribute('x-html')
-		name.textContent = user.name
-		username.textContent = user.username
-		email.textContent = user.email
-		table.appendChild(clone)
+	const data = await fetch(
+		`https://api.dotenx.com/public/database/query/select/project/${projectTag}/table/${tableName}`,
+		{
+			body: JSON.stringify({ columns: [] }),
+			method: 'POST',
+		}
+	).then((res) => res.json())
+
+	head.style.gridTemplateColumns = `repeat(${Object.keys(data.rows[0]).length - 1}, 1fr)`
+	Object.keys(data.rows[0]).forEach((column) => {
+		if (column === 'creator_id') return
+		const cell = document.createElement('div')
+		cell.textContent = column
+		head.appendChild(cell)
+	})
+
+	data.rows.forEach((row) => {
+		const cells = rowTemplate.cloneNode(true)
+		cells.innerHTML = ''
+		cells.style.gridTemplateColumns = `repeat(${Object.keys(row).length - 1}, 1fr)`
+		Object.keys(row).forEach((column) => {
+			if (column === 'creator_id') return
+			const cell = document.createElement('div')
+			cell.textContent = row[column]
+			cells.appendChild(cell)
+		})
+		table.appendChild(cells)
 	})
 })()
