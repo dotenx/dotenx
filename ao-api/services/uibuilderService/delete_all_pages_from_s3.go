79592980242a4d6c4f9d328f9a4956240dc8be46
Hojjat-1 <hojjat.hajizadeh@utopiops.com>
2024-02-27 13:43:27 +0330

    Fix bug of deleting all pages from S3 bucket

--- ao-api/services/uibuilderService/delete_all_pages_from_s3.go
+++ ao-api/services/uibuilderService/delete_all_pages_from_s3.go
@@ -9,18 +9,30 @@ import (
 
 func (ps *uibuilderService) DeleteAllPagesFromS3(projectDomain models.ProjectDomain) (err error) {
 
-	var bucket, prefix string
-	if !projectDomain.CertificateIssued { // We use certificate issuance status as an indicator that the external domain is verified and ready to be used or not
-		bucket = config.Configs.UiBuilder.S3Bucket
-		prefix = projectDomain.InternalDomain + ".web" + "/"
-	} else {
-		bucket = projectDomain.S3Bucket
-		prefix = "/"
-	}
-	err = utils.DeleteS3Folder(bucket, prefix)
+	pages, err := ps.ListPages(projectDomain.AccountId, projectDomain.ProjectTag)
 	if err != nil {
 		logrus.Error(err.Error())
 		return
+	}
+
+	for _, pageName := range pages {
+		var bucket, prefix string
+		if !projectDomain.CertificateIssued { // We use certificate issuance status as an indicator that the external domain is verified and ready to be used or not
+			bucket = config.Configs.UiBuilder.S3Bucket
+			prefix = projectDomain.InternalDomain + ".web" + "/"
+		} else {
+			bucket = projectDomain.S3Bucket
+			prefix = ""
+		}
+		fileExtensions := []string{".html", ".css", ".js"}
+		for _, ext := range fileExtensions {
+			fileName := prefix + pageName + ext
+			err = utils.DeleteObject(bucket, fileName)
+			if err != nil {
+				logrus.Error(err.Error())
+				return
+			}
+		}
 	}
 
 	return
